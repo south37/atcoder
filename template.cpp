f3de2019a6d148781c4a6ac98776f7f07c8b23fa
Nao Minami <south37777@gmail.com>
2019-08-04 20:51:05 +0900

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int main(int argc, char** argv) {
+  int n;
+  cin >> n;
+
+  cout << n << endl;
+}
