d0e63ea7f4698e21e6718e52d66ff3b8be1da9aa
Nao Minami <south37777@gmail.com>
2019-08-17 16:08:33 +0900

    Add reachable

--- abc137/e/e.cpp
+++ abc137/e/e.cpp
@@ -8,13 +8,37 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < n; ++i)
 #define all(s) s.begin(), s.end()
 
-#define INF INT64_MAX
+#define INF 123456789
 
 struct edge {
   int from;
   int to;
   int cost;
 };
+
+vector<int> to_from[2505];
+vector<int> from_of_to[2505];
+bool reachable_from_s[2505];
+bool reachable_to_g[2505];
+bool reachable[2505];
+
+void dfsFromS(int v) {
+  if (reachable_from_s[v]) { return; } // already reached.
+
+  reachable_from_s[v] = true;
+  for (int u : to_from[v]) {
+    dfsFromS(u);
+  }
+}
+
+void dfsFromG(int v) {
+  if (reachable_to_g[v]) { return; } // already reached.
+
+  reachable_to_g[v] = true;
+  for (int u : from_of_to[v]) {
+    dfsFromG(u);
+  }
+}
 
 int main(int argc, char** argv) {
   // N .. vertex
@@ -26,8 +50,12 @@ int main(int argc, char** argv) {
   rep(i, M) {
     edge e;
     cin >> e.from >> e.to >> e.cost;
+    --e.from; // Convert index from [1, N] to [0, N-1].
+    --e.to;   // Convert index from [1, N] to [0, N-1].
     e.cost = - (e.cost - P);
     edges.push_back(e);
+    to_from[e.from].push_back(e.to);
+    from_of_to[e.to].push_back(e.from);
   }
 
   // For Debug
@@ -35,27 +63,38 @@ int main(int argc, char** argv) {
   //   cout << edges[i].from << ", " << edges[i].to << ", " << edges[i].cost << endl;
   // }
 
+  dfsFromS(0);  // For initialization of reachable_from_s
+  dfsFromG(N - 1);  // For initialization of reachable_to_g
+  rep(i, N) {  // For initialization of reachable
+    reachable[i] = reachable_from_s[i] && reachable_to_g[i];
+  }
+
+  // NOTE: We focus only reachable vetices.
+
   bool negative_existence = false;
-  vector<int64_t> d(N + 1, INF);  // d[0] is dummy. Initialize with INF.
-  d[1] = 0;  // Set 0 to start point.
+  vector<int64_t> d(N, INF);  // Initialize with INF.
+  d[0] = 0;  // Set 0 to start point.
   rep(i, N) {
     rep(j, M) {
       edge e = edges[j];
-      if ((d[e.from] < INF) && (d[e.to] > d[e.from] + e.cost)) {  // If d[e.from] == INF, it can't be reached.
+      if (d[e.to] > d[e.from] + e.cost) {
+        if (!reachable[e.from]) { continue; }
+        if (!reachable[e.to]) { continue; }
+
         d[e.to] = d[e.from] + e.cost;
 
         // For Deubg
         // cout << i << " loop: " << endl;
         // rep(i, N) {
-        //   if (d[i + 1] == INF) {
+        //   if (d[i] == INF) {
         //     cout << "INF ";
         //   } else {
-        //     cout << d[i + 1] << " ";
+        //     cout << d[i] << " ";
         //   }
         // }
         // cout << endl;
 
-        if ((i == N - 1) && (e.to == N)) { // Check loop count. if reached to N times, it means the existence of loop.
+        if (i == N - 1) { // Check loop count. if reached to N times, it means the existence of loop.
           negative_existence = true;
           break;
         }
@@ -65,14 +104,11 @@ int main(int argc, char** argv) {
 
   if (negative_existence) {
     cout << -1 << endl;
-  } else if (d[N] == INF) { // Goal can't be reached.
+  } else if (d[N - 1] == INF) { // Goal can't be reached.
     cout << -1 << endl;
   } else {
-    if (d[N] > 0) {
-      cout << 0 << endl;
-    } else {
-      cout << -d[N] << endl;
-    }
+    int r = -d[N - 1];
+    cout << max(0, r) << endl;
   }
 
   return 0;
