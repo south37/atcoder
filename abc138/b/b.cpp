a8d7d47287800c0d0af36b7c0704d1ff34c89007
Nao Minami <south37777@gmail.com>
2019-08-18 20:33:16 +0900

    Add solutions

---
+++ abc138/b/b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int main(int argc, char** argv) {
+  int n;
+  cin >> n;
+
+  cout << n << endl;
+}
