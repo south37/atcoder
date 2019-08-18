9a67336e3464167f4238f4aad9a295b1a1e1e517
Nao Minami <south37777@gmail.com>
2019-08-18 15:32:13 +0900

    Update

--- abc135/d_digits_parade/digits_parade.cpp
+++ abc135/d_digits_parade/digits_parade.cpp
@@ -4,18 +4,17 @@ using namespace std;
 
 #define rep(i, n) for(int i = 0; i < n; ++i)
 
-static const uint64_t MOD = 1000000007LL;
+#define MOD 1000000007LL;
 
-int n;
-char s[123456];
 uint64_t dp[123456][13];
 
 int main(int argc, char** argv) {
   int i, j, k;
   uint64_t res = 0;
 
-  scanf("%s", s);
-  n = strlen(s);
+  string s;
+  cin >> s;
+  int n = s.size();
 
   dp[0][0] = 1;
   rep(i, n) {
