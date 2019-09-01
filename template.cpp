082bb47525305738a82482ccf231731c5b42c436
Nao Minami <south37777@gmail.com>
2019-09-01 23:18:09 +0900

    Add P and triple

--- template.cpp
+++ template.cpp
@@ -11,6 +11,12 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < n; ++i)
 #define all(s) s.begin(), s.end()
 
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef pair<int, int> P;
+typedef tuple<int, int, int> triple;
+
 int main(int argc, char** argv) {
   int n;
   cin >> n;
