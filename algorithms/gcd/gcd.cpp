dc1114cb24ca2f24eb13787bc85af6da448f611d
Nao Minami <south37777@gmail.com>
2019-09-01 23:38:08 +0900

    Add gcd

---
+++ algorithms/gcd/gcd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef pair<int, int> P;
+typedef tuple<int, int, int> triple;
+
+int gcd(int a, int b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
+
+int main(int argc, char** argv) {
+  int n, m;
+  cin >> n >> m;
+
+  cout << gcd(n, m) << endl;
+}
