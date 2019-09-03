86771624de184d453469740fb7b3f4a7926b2825
Nao Minami <south37777@gmail.com>
2019-09-03 23:20:20 +0900

    Add partial_sum_min

---
+++ playground/dp/partial_sum_min/partial_sum_min.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef pair<int, int> P;
+typedef tuple<int, int, int> triple;
+
+int gcd(int a, int b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
+
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) { return false; }
+  }
+  return n != 1;
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const int INF = 1e3;
+
+int dp[123][12345];
+
+int main(int argc, char** argv) {
+  int N, A;
+  cin >> N >> A;
+  vector<int> a(N);
+  rep(i, N) {
+    cin >> a[i];
+  }
+
+  rep(i, 123) {
+    rep(j, 12345) {
+      dp[i][j] = INF;
+    }
+  }
+  dp[0][0] = 0;
+
+  rep(i, N) {
+    rep(j, A+1) {
+      if (j >= a[i]) {
+        dp[i+1][j] = min(dp[i][j], dp[i][j-a[i]] + 1);
+      } else {
+        dp[i+1][j] = dp[i][j];
+      }
+    }
+  }
+
+  if (dp[N][A] == INF) {
+    cout << -1 << endl;
+  } else {
+    cout << dp[N][A] << endl;
+  }
+}
