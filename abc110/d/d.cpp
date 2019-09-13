71a021bce69e4a64cbf0745008fc7f227ce59660
Nao Minami <south37777@gmail.com>
2019-09-13 22:04:15 +0900

    Add d.cpp

---
+++ abc110/d/d.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef pair<int, int> P;
+typedef tuple<int, int, int> triple;
+typedef long long ll;
+
+int gcd(int a, int b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
+
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) { return false; }
+  }
+  return n != 1;
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+ll powmod(ll x, ll y) {
+  ll r = 1;
+  while (y) {
+    if (y & 1) {
+      r *= x;
+      r %= MOD;
+    }
+    x *= x;
+    x %= MOD;
+    y >>= 1;
+  }
+  return r;
+}
+
+ll fac[200010], facinv[200010];  // 200010 is Larger than 1e5 + 1e5.
+void cominit() {
+  fac[0] = facinv[0] = 1;
+  ll c = 1;
+  for (int i = 1; i < 200010; ++i) {
+    c *= i;
+    c %= MOD;
+    fac[i] = c;
+    facinv[i] = powmod(c, MOD-2);
+  }
+}
+
+ll combination(ll n, ll k) {
+  return (fac[n] * facinv[k] % MOD) * facinv[n-k] % MOD;
+}
+
+ll permutation(ll n, ll k) {
+  return (fac[n] * facinv[k] % MOD);
+}
+
+int main(int argc, char** argv) {
+  int N, M;
+  cin >> N >> M;
+
+  map<int, int> factors;
+  for (int i = 2; i * i <= M; ++i) {
+    while (M % i == 0) {
+      ++factors[i];
+      M /= i;
+    }
+  }
+  if (M > 1) {
+    ++factors[M];
+  }
+
+  cominit();
+  ll r = 1;
+  for (auto x : factors) {
+    r *= combination(x.second + N - 1, N - 1);
+    r %= MOD;
+  }
+
+  cout << r << endl;
+}
