58f2c0e5db714b588ee97a74e46d09f01b6871f8
Nao Minami <south37777@gmail.com>
2019-09-22 00:20:39 +0900

    Add SegmentTree

---
+++ algorithms/SegmentTree/RMQ/RMQ.cpp
@@ -0,0 +1,90 @@
+// Ari-hon. 3-3. P155.
+// RMQ (Range Minimum Query) by Segment Tree.
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+template <typename T>
+class SegTree {
+public:
+  SegTree(int _n) {
+    n = 1;
+    while (n < _n) { n *= 2; }
+    dat = vector<T>(2 * n - 1, INF);
+  }
+
+  void update(int k, T a) {
+    k += n - 1;
+    dat[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+    }
+  }
+
+  // Calculate the min of [a, b)
+  T query(int a, int b) {
+    return _query(a, b, 0, 0, n);
+  }
+
+  // Calculate the min of [a, b)
+  // k is the index (dat[k]). This is matched to [l, r)
+  T _query(int a, int b, int k, int l, int r) {
+    // The intersection of [a, b) and [r, l) is blank.
+    if (r <= a || b <= l) { return INF; }
+
+    if (a <= l && r <= b) {  // [r, l) is completely included in [a, b)
+      return dat[k];
+    } else {
+      int vl = _query(a, b, k * 2 + 1, l, (l + r) / 2);
+      int vr = _query(a, b, k * 2 + 2, (l + r) / 2, r);
+      return min(vl, vr);
+    }
+  }
+
+private:
+  int n;
+  vector<T> dat;
+};
+
+int main(int argc, char** argv) {
+  int arr[] = { 1, 3, 2, 7, 9, 11 };
+  int n = 6;
+
+  SegTree<int> st(n);
+  rep(i, n) {
+    st.update(i, arr[i]);
+  }
+
+  cout << st.query(1, 5) << endl;
+  cout << st.query(0, 4) << endl;
+  cout << st.query(3, 5) << endl;
+}
