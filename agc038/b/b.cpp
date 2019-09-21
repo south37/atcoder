3c6258f56bad0b6bbf4a501952bd866b9fd7d122
Nao Minami <south37777@gmail.com>
2019-09-21 20:18:44 +0900

    Add agc038 templates

---
+++ agc038/b/b.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+int gcd(int a, int b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
+
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); ++i) {
+    if (n % i == 0) { return false; }
+  }
+  return n != 1;
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+ll powmod(ll x, ll n) { // like pow(x, n)
+  ll r = 1;
+  while (n) {
+    if (n & 1) {
+      r = r * x % MOD;
+    }
+    x = x * x % MOD;
+    n >>= 1;
+  }
+  return r;
+}
+
+const int COM_MAX = 500010;
+ll fac[COM_MAX], facinv[COM_MAX], inv[COM_MAX];
+void COMinit() {
+  fac[0] = fac[1] = 1;
+  facinv[0] = facinv[1] = 1;
+  inv[1] = 1;
+  for(int i = 2; i < COM_MAX; ++i) {
+    fac[i] = fac[i-1] * i % MOD;
+    inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+    facinv[i] = facinv[i-1] * inv[i] % MOD;
+  }
+}
+
+ll COM(ll n, ll k) {
+  return (fac[n] * facinv[k] % MOD) * facinv[n-k] % MOD;
+}
+
+ll PERM(ll n, ll k) {
+  return (fac[n] * facinv[k] % MOD);
+}
+
+int main(int argc, char** argv) {
+  int n;
+  cin >> n;
+
+  cout << n << endl;
+}
