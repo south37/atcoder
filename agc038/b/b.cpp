d9a862f1bda77ece53e03a57c1b2d1f7fa29721f
Nao Minami <south37777@gmail.com>
2019-09-21 22:24:53 +0900

    Add min

--- agc038/b/b.cpp
+++ agc038/b/b.cpp
@@ -217,6 +217,108 @@ int* constructST(int arr[], int n)
     return st;
 }
 
+// A utility function to get minimum of two numbers
+int minVal(int x, int y) { return (x < y)? x: y; }
+
+// A utility function to get the
+// middle index from corner indexes.
+int getMidForMin(int s, int e) { return s + (e -s)/2; }
+
+/* A recursive function to get the
+minimum value in a given range
+of array indexes. The following
+are parameters for this function.
+
+    st --> Pointer to segment tree
+    index --> Index of current node in the
+           segment tree. Initially 0 is
+           passed as root is always at index 0
+    ss & se --> Starting and ending indexes
+                of the segment represented
+                by current node, i.e., st[index]
+    qs & qe --> Starting and ending indexes of query range */
+int RMQUtil(int *st, int ss, int se, int qs, int qe, int index)
+{
+    // If segment of this node is a part
+    // of given range, then return
+    // the min of the segment
+    if (qs <= ss && qe >= se)
+        return st[index];
+
+    // If segment of this node
+    // is outside the given range
+    if (se < qs || ss > qe)
+        return INT_MAX;
+
+    // If a part of this segment
+    // overlaps with the given range
+    int mid = getMidForMin(ss, se);
+    return minVal(RMQUtil(st, ss, mid, qs, qe, 2*index+1),
+                RMQUtil(st, mid+1, se, qs, qe, 2*index+2));
+}
+
+// Return minimum of elements in range
+// from index qs (quey start) to
+// qe (query end). It mainly uses RMQUtil()
+int getMin(int *st, int n, int qs, int qe)
+{
+    // Check for erroneous input values
+    if (qs < 0 || qe > n-1 || qs > qe)
+    {
+        cout<<"Invalid Input";
+        return -1;
+    }
+
+    return RMQUtil(st, 0, n-1, qs, qe, 0);
+}
+
+// A recursive function that constructs
+// Segment Tree for array[ss..se].
+// si is index of current node in segment tree st
+int constructSTForMinUtilForMin(int arr[], int ss, int se,
+                                int *st, int si)
+{
+    // If there is one element in array,
+    // store it in current node of
+    // segment tree and return
+    if (ss == se)
+    {
+        st[si] = arr[ss];
+        return arr[ss];
+    }
+
+    // If there are more than one elements,
+    // then recur for left and right subtrees
+    // and store the minimum of two values in this node
+    int mid = getMidForMin(ss, se);
+    st[si] = minVal(constructSTForMinUtilForMin(arr, ss, mid, st, si*2+1),
+                    constructSTForMinUtilForMin(arr, mid+1, se, st, si*2+2));
+    return st[si];
+}
+
+/* Function to construct segment tree
+from given array. This function allocates
+memory for segment tree and calls constructSTForMinUtilForMin() to
+fill the allocated memory */
+int *constructSTForMin(int arr[], int n)
+{
+    // Allocate memory for segment tree
+
+    //Height of segment tree
+    int x = (int)(ceil(log2(n)));
+
+    // Maximum size of segment tree
+    int max_size = 2*(int)pow(2, x) - 1;
+
+    int *st = new int[max_size];
+
+    // Fill the allocated memory st
+    constructSTForMinUtilForMin(arr, 0, n-1, st, 0);
+
+    // Return the constructed segment tree
+    return st;
+}
+
 
 int main(int argc, char** argv) {
   int N, K;
@@ -237,7 +339,11 @@ int main(int argc, char** argv) {
     arr[i] = P[i];
   }
 
-  // Build segment tree from given array
-  int* st = constructST(arr, N);
-  cout << getMax(st, N, 0, 2) << endl;
+  // Build segment tree from given array for max
+  int* st_max = constructST(arr, N);
+  cout << getMax(st_max, N, 0, 2) << endl;
+
+  // Build segment tree from given array for min
+  int* st_min = constructSTForMin(arr, N);
+  cout << getMin(st_min, N, 0, 2) << endl;
 }
