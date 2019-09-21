5960e7bca7dcfb10af88386b789ccc447bd2389e
Nao Minami <south37777@gmail.com>
2019-09-21 20:39:49 +0900

    Add agc034/b

---
+++ agc034/b/b.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+int gcd(int a, int b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
+
+bool prime(int n) {
+  for (int i = 2; i <= sqrt(n); ++i) {
+    if (n % i == 0) { return false; }
+  }
+  return n != 1;
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  string s;
+  cin >> s;
+
+  ll cnt = 0;
+  ll ans = 0;
+  for (int i = 0; i < s.size(); ++i) {
+    if (s[i] == 'B' && s[i+1] == 'C') {  // "BC" pair.
+      ans += cnt;
+      ++i; // We consider this as pair, so skip C.
+    } else if (s[i] == 'A') {
+      ++cnt;
+    } else {  // "B" or "C" alone.
+      cnt = 0;  // reset count.
+    }
+  }
+
+  cout << ans << endl;
+}
