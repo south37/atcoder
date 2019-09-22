d8709d91d2551788fc094205aa5e1c0e44e2b14b
Nao Minami <south37777@gmail.com>
2019-09-22 13:33:17 +0900

    Update UnionFind

--- algorithms/UnionFind/UnionFind.cpp
+++ algorithms/UnionFind/UnionFind.cpp
@@ -27,61 +27,12 @@ typedef pair<ll, ll> P;
 typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
-int gcd(int a, int b) {
-  if (a < b) { swap(a, b); }
-  if (b == 0) { return a; }
-  return gcd(b, a % b);
-}
-
-bool prime(int n) {
-  for (int i = 2; i <= sqrt(n); ++i) {
-    if (n % i == 0) { return false; }
-  }
-  return n != 1;
-}
-
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
-
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-ll powmod(ll x, ll n) { // like pow(x, n)
-  ll r = 1;
-  while (n) {
-    if (n & 1) {
-      r = r * x % MOD;
-    }
-    x = x * x % MOD;
-    n >>= 1;
-  }
-  return r;
-}
-
-const int COM_MAX = 500010;
-ll fac[COM_MAX], facinv[COM_MAX], inv[COM_MAX];
-void COMinit() {
-  fac[0] = fac[1] = 1;
-  facinv[0] = facinv[1] = 1;
-  inv[1] = 1;
-  for(int i = 2; i < COM_MAX; ++i) {
-    fac[i] = fac[i-1] * i % MOD;
-    inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-    facinv[i] = facinv[i-1] * inv[i] % MOD;
-  }
-}
-
-ll COM(ll n, ll k) {
-  return (fac[n] * facinv[k] % MOD) * facinv[n-k] % MOD;
-}
-
-ll PERM(ll n, ll k) {
-  return (fac[n] * facinv[k] % MOD);
-}
-
 class UnionFind {
 public:
-  UnionFind(int size) : par(size, -1) {}
+  UnionFind(int size) : par(size, -1), rnk(size, 0) {}
 
   bool same(int x, int y) {
     return root(x) == root(y);
@@ -89,9 +40,13 @@ public:
   void unite(int x, int y) {
     x = root(x); y = root(y);
     if (x == y) return;
-    if (x > y) { swap(x, y); }
-    // here, x < y
-    par[y] = x;
+
+    if (rnk[x] < rnk[y]) {
+      par[x] = y;
+    } else {
+      par[y] = x;
+      if (rnk[x] == rnk[y]) { ++rnk[x]; }
+    }
   }
   int root(int x) {
     if (par[x] < 0) {
@@ -103,6 +58,7 @@ public:
 
 private:
   vector<int> par;
+  vector<int> rnk;
 };
 
 int main(int argc, char** argv) {
