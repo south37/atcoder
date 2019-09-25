cc851ddc4b8bd6c20e85d5996ec1d49cd90541cc
Nao Minami <south37777@gmail.com>
2019-09-25 22:59:45 +0900

    Add other operations

--- abc139/f/f.cpp
+++ abc139/f/f.cpp
@@ -34,11 +34,14 @@ struct V {
 
   V(double x = 0, double y = 0) : x(x), y(y) {}
 
-  V& operator+=(const V& v) {
-    x += v.x; y += v.y;
-    return *this;
-  }
+  V& operator+=(const V& v) { x += v.x; y += v.y; return *this; }
   V& operator+(const V& v) const { return V(*this) += v; }
+  V& operator-=(const V& v) { x -= v.x; y -= v.y; return *this; }
+  V& operator-(const V& v) const { return V(*this) -= v; }
+  V& operator*=(double s) { x *= s; y *= s; return *this; }
+  V& operator*(double s) const { return V(*this) *= s; }
+  V& operator/=(double s) { x /= s; y /= s; return *this; }
+  V& operator/(double s) const { return V(*this) /= s; }
   double dot(const V& v) const { return x*v.x + y*v.y; }
   double cross(const V& v) const { return x*v.y - y*v.x; }
   double norm() const { return sqrt(norm2()); }
