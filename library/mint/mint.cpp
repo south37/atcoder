1aae241cc01153c202a6fddc0207469565229393
Nao Minami <south37777@gmail.com>
2019-09-27 06:45:00 +0900

    Add mint

---
+++ library/mint/mint.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+ll MOD;
+
+// Mod int
+struct mint {
+  ll x;
+  mint(ll x = 0) : x(x % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+int main(int argc, char** argv) {
+  // int p;
+  // cin >> p;
+
+  MOD = 13;
+  mint p(10);
+  cout << (p + 15).x << endl;   // 12 (25 % 13)
+  cout << (p - 15).x << endl;   // 8  (-5 % 13)
+  cout << (p * 2).x << endl;    // 7  (20 % 13)
+  cout << (p.pow(3)).x << endl; // 12 (1000 % 13)
+  cout << (p / 3).x << endl;    // 12 (12 * 3 = 10 (36 % 13))
+}
