7680d18d5d1ed116a2c6e561e113086db14ae226
Nao Minami <south37777@gmail.com>
2019-09-27 07:26:39 +0900

    Update

--- algorithms/UnionFind/UnionFind.cpp
+++ algorithms/UnionFind/UnionFind.cpp
@@ -18,9 +18,6 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < n; ++i)
 #define all(s) s.begin(), s.end()
 
-int dx[] = { 1, -1, 0, 0 };
-int dy[] = { 0, 0, 1, -1 };
-
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<ll, ll> P;
