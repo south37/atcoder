22b3cf2fc8ee3fb4689e4a7f8c673deb9ba02feb
Nao Minami <south37777@gmail.com>
2019-09-27 07:11:11 +0900

    Add abc137/f

--- abc137/f/f.cpp
+++ abc137/f/f.cpp
@@ -29,26 +29,17 @@ typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
 const ll INF = 1e9;
-const ll MOD = 1000000007;  // 1e9 + 7
-
-ll powmod(ll x, ll n) { // like pow(x, n)
-  ll r = 1;
-  while (n) {
-    if (n & 1) {
-      r = r * x % MOD;
-    }
-    x = x * x % MOD;
-    n >>= 1;
-  }
-  return r;
-}
+ll MOD;
 
 // Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
 struct mint {
   ll x;
-  mint(ll x = 0) : x(x % MOD) {}
+  mint(ll x = 0) : x(x % MOD) {
+    if (x < 0) { x += MOD; }
+  }
   mint& operator+= (const mint a) {
-    if ((x += a.x) >= MOD) x -= MOD;
+    if ((x += a.x) >= MOD) x %= MOD;
     return *this;
   }
   mint operator+ (const mint a) const {
@@ -56,7 +47,7 @@ struct mint {
     return res += a;
   }
   mint& operator-= (const mint a) {
-    if ((x += MOD - a.x) >= MOD) x -= MOD;
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
     return *this;
   }
   mint operator- (const mint a) const {
@@ -78,12 +69,64 @@ struct mint {
     if (t & 1) a *= *this;
     return a;
   }
-}
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// Combination mod prime.
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct Combination {
+  vector<mint> fact, ifact;
+  Combination(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n < MOD); // n must be lower than MOD.
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) { fact[i] = fact[i-1] * i; }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) { ifact[i-1] = ifact[i] * i; }
+  }
+  mint operator() (int n, int k) {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[k] * ifact[n-k];
+  }
+};
 
 int main(int argc, char** argv) {
-  // int p;
-  // cin >> p;
+  int p;
+  cin >> p;
+  MOD = p;
 
-  int p = 10;
-  cout << n << endl;
+  vector<int> a(p);
+  rep(i, p) {
+    cin >> a[i];
+  }
+
+  Combination c(p-1);
+  vector<mint> b(p);
+  rep(i, p) {
+    if (!a[i]) { continue; }
+    // 1 - (x-i) ** (p-1) = b[p-1] * x ** (p-1) + b[p-2] * x ** (p-2) + ... + b[0]
+    b[0] += 1;
+    mint pw = 1; // pw = (-i) ** (p - 1 - j) in each step.
+    for (int j = p - 1; j >= 0; --j) {
+      b[j] -= pw * c(p-1, j);
+      pw *= -i;
+    }
+  }
+  rep(i, p) {
+    cout << b[i].x;
+    if (i != p-1) {
+      cout << " ";
+    }
+  }
+  cout << endl;
 }
