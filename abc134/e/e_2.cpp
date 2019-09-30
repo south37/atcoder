252507f539201de53835badff9eb3dec81a2c6e4
Nao Minami <south37777@gmail.com>
2019-09-30 23:06:50 +0900

    Add another answer

---
+++ abc134/e/e_2.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+ll powmod(ll x, ll n) { // like pow(x, n)
+  ll r = 1;
+  while (n) {
+    if (n & 1) {
+      r = r * x % MOD;
+    }
+    x = x * x % MOD;
+    n >>= 1;
+  }
+  return r;
+}
+
+int main(int argc, char** argv) {
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N) {
+    cin >> A[i];
+  }
+
+  deque<int> d;
+  rep(i, N) {
+    int p = lower_bound(all(d), A[i]) - d.begin();
+    if (p == 0) {
+      d.push_front(A[i]);
+    } else {
+      d[p-1] = A[i];
+    }
+  }
+  int ans = d.size();
+  cout << ans << endl;
+}
