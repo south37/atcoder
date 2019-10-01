5538df6139bdfbf83b68acda6b4f83f6602362fd
Nao Minami <south37777@gmail.com>
2019-10-02 08:44:59 +0900

    Add f.cpp

---
+++ abc133/f/f.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+ll powmod(ll x, ll n) { // like pow(x, n)
+  ll r = 1;
+  while (n) {
+    if (n & 1) {
+      r = r * x % MOD;
+    }
+    x = x * x % MOD;
+    n >>= 1;
+  }
+  return r;
+}
+
+int main(int argc, char** argv) {
+  int n;
+  cin >> n;
+
+  cout << n << endl;
+}
