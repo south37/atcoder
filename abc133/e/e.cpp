86f4600bc281a7a9086a2faa5d86fa05297d9ddd
Nao Minami <south37777@gmail.com>
2019-10-02 08:04:53 +0900

    Add abc133/e

--- abc133/e/e.cpp
+++ abc133/e/e.cpp
@@ -40,9 +40,125 @@ ll powmod(ll x, ll n) { // like pow(x, n)
   return r;
 }
 
-int main(int argc, char** argv) {
-  int n;
-  cin >> n;
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
 
-  cout << n << endl;
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// Combination mod prime.
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct Combination {
+  vector<mint> fact, ifact;
+  Combination(int n) : fact(n + 1), ifact(n + 1) {
+    assert(n < MOD); // n must be lower than MOD.
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) { fact[i] = fact[i-1] * i; }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) { ifact[i-1] = ifact[i] * i; }
+  }
+  mint operator() (int n, int k) {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[k] * ifact[n-k];
+  }
+  mint perm(int n, int k) {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[n-k];
+  }
+};
+
+Combination comb(100010);
+int K;
+vector< vector<int> > tree;
+
+// Return the number of pattern
+mint dfs(int v, int p) {
+  // We want to return nk_P_j
+  int nk; // number of colors we can use.
+  int j; // number of vertices we have to paint.
+
+  if (p == -1) { // root
+    nk = K;
+    j  = tree[v].size() + 1; // We must paint the root. This is +1.
+  } else {
+    nk = K - 2;
+    j  = tree[v].size() - 1; // Extract parent. This is -1.
+  }
+  // cout << "v: "<<v<<", p: "<<p << endl;
+  // cout << "nk: "<<nk<<", j: "<<j << endl;
+
+  if (nk < j) { // number of color is smaller
+    return 0;
+  }
+
+  mint res = comb.perm(nk, j);
+  for (auto u : tree[v]) {
+    if (u == p) { continue; }
+    res *= dfs(u, v);
+  }
+  return res;
+}
+
+int main(int argc, char** argv) {
+  int N;
+  cin >> N >> K;
+  tree.resize(N);
+  rep(i, N - 1) {
+    int a, b;
+    cin >> a >> b;
+    --a; --b;
+    tree[a].push_back(b);
+    tree[b].push_back(a);
+  }
+  // rep(i, N) {
+  //   for (auto x : tree[i]) {
+  //     cout << i << "->" << x << endl;
+  //   }
+  // }
+  mint ans = dfs(0, -1); // dfs from root (0). -1 indicates the root (no-parent).
+  cout << ans.x << endl;
 }
