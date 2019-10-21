ecfd3f85fcf4bebb824e4ba0edbc40af3ae1d941
Nao Minami <south37777@gmail.com>
2019-10-21 18:50:17 +0900

    Add abc127/e

---
+++ abc127/e/e.cpp
@@ -0,0 +1,148 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// Combination mod prime.
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct Combination {
+  vector<mint> fact, ifact;
+  Combination(int n) { init(n); }
+  void init(int n) {
+    assert(n < MOD); // n must be lower than MOD.
+
+    fact.resize(n + 1);
+    ifact.resize(n + 1);
+
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) { fact[i] = fact[i-1] * i; }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) { ifact[i-1] = ifact[i] * i; }
+  }
+  mint operator() (int n, int k) const {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[k] * ifact[n-k];
+  }
+  mint perm(int n, int k) const {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[n-k];
+  }
+};
+
+// int main(int argc, char** argv) {
+//   MOD = 13;
+//
+//   Combination c(12);
+//   cout << c(12, 0).x << endl;  // 1  = 1 % 13
+//   cout << c(12, 1).x << endl;  // 12 = 12 % 13
+//   cout << c(12, 2).x << endl;  // 1  = 66 % 13 = (12 * 11 / 2) % 13
+//   cout << c(12, 3).x << endl;  // 12 = 220 % 13 = (12 * 11 * 10 / (3 * 2 * 1)) % 13
+//   cout << c(12, 11).x << endl; // 12 = 12 % 13
+//   cout << c(12, 12).x << endl; // 1  = 12 % 13
+//
+//   cout << c.perm(12, 3).x << endl; // 7 = 12 * 11 * 10 % 13
+// }
+
+// - answer
+//   - x
+//     - factor: (N-2)C(K-2)
+//     - each d: (N-d) * M*2
+//       - d: [1, N)
+//   - y
+//     - factor: (N-2)C(K-2)
+//     - each d: (M-d) * N*2
+//       - d: [1, M)
+
+int main(int argc, char** argv) {
+  ll N, M, K;
+  cin >> N >> M >> K;
+
+  mint x(0);
+  for (ll d = 1; d < N; ++d) { // d: |xi-xj|
+    x += mint(d) * mint(N-d) * mint(M) * mint(M);
+  }
+  mint y(0);
+  for (ll d = 1; d < M; ++d) { // d: |yi-yj|
+    y += mint(d) * mint(M-d) * mint(N) * mint(N);
+  }
+
+  Combination c(N * M);
+  mint ans = (x + y) * c(N * M-2, K-2);
+
+  cout << ans.x << endl;
+}
