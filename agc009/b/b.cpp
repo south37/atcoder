7ef87a8377f5d34040582ed20fa549a6f0e4c2ca
Nao Minami <south37777@gmail.com>
2019-11-03 21:23:51 +0900

    Update

--- agc009/b/b.cpp
+++ agc009/b/b.cpp
@@ -28,9 +28,56 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+vector< vector<ll> > tree;
+
+ll dfs(int p) {
+  if (tree[p].size() == 0) {
+    return 0;
+  }
+
+  vector<ll> d;
+  for (auto u : tree[p]) {
+    d.push_back(dfs(u));
+  }
+  sort(all(d));
+  reverse(all(d));
+  // cout << "d: ";
+  // rep(i, d.size()) {
+  //   cout << d[i] << " ";
+  // }
+  // cout << endl;
+
+  int res = 0;
+  rep(i, d.size()) {
+    int c = d[i] + i + 1;
+    if (res < c) {
+      res = c;
+    }
+  }
+  // cout << "res: " << res << endl;
+
+  return res;
+}
+
 int main(int argc, char** argv) {
   ll N;
   cin >> N;
+  tree.resize(N);
+  for (int i = 1; i < N; ++i) {
+    ll j;
+    cin >> j;
+    --j; // 0-indexed
+    tree[j].push_back(i);
+  }
 
-  cout << N << endl;
+  // For Debug
+  // rep(i, N) {
+  //   cout << i + 1 << ": ";
+  //   for (auto j : tree[i]) {
+  //     cout << j + 1 << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  cout << dfs(0) << endl;
 }
