028d71e8332e2b78dfa675be644a4177c2b977ea
Nao Minami <south37777@gmail.com>
2019-11-04 01:21:35 +0900

    Invalid answer

--- agc040/b/b.cpp
+++ agc040/b/b.cpp
@@ -39,12 +39,61 @@ typedef pair<ll, ll> P;
 typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
-const ll INF = 1e9;
+const ll INF = 1e12;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 int main(int argc, char** argv) {
   ll N;
   cin >> N;
+  vector<P> lrs;
+  rep(i, N) {
+    ll L, R;
+    cin >> L >> R;
+    lrs.emplace_back(L, R);
+  }
+  sort(all(lrs));
 
-  cout << N << endl;
+  for (auto x : lrs) { cout << x.first << ", " << x.second << " " << endl; }
+
+  vector<ll> min_r(N+1, INF); // min_r[i+1] .. min R[i] in [0, i]. min_r[0] = 0
+  rep(i, N) {
+    min_r[i+1] = min(min_r[i], lrs[i].second);
+  }
+  vector<ll> min_r2(N+1, INF); // min_r2[i-1] .. min R[i] in [i, N-1]. min_r[N-1] = 0
+  for (int i = N; i > 0; --i) {
+    min_r2[i-1] = min(min_r2[i], lrs[i-1].second);
+  }
+  vector<ll> max_l(N+1); // min_l[i-1] .. max L[i] in [i, N-1]. min_l[N-1] = 0
+  for (int i = N; i > 0; --i) {
+    max_l[i-1] = max(max_l[i], lrs[i-1].first);
+  }
+  cout << "min_r: ";
+  printvec(min_r);
+  cout << "min_r2: ";
+  printvec(min_r2);
+  cout << "max_l: ";
+  printvec(max_l);
+
+  ll ans = 0;
+
+  // Try all splits.
+  rep(i, N-1) { // Try N-1 times.
+    ll L, R;
+    tie(L, R) = lrs[i];
+
+    ll left = min_r[i+1] - L + 1; // [0, i]
+    ll right = min_r2[i] - max_l[i] + 1; // [i+1, N-1]
+    ll c = left + right;
+
+    cout << i << ": " << endl;
+    cout << "left: " << left << "=" << "("<<L<<", "<<min_r[i+1]<<") "<< endl;
+    cout << "right: " << right << "=" << "("<<max_l[i]<<", "<<min_r2[i]<<") "<< endl;
+    cout << "c: " << c << endl;
+
+    if (ans < c) {
+      ans = c;
+    }
+  }
+
+  cout << ans << endl;
 }
