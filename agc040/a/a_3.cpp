dac767fb51c14c14e74561854619b455b55f1465
Nao Minami <south37777@gmail.com>
2019-11-04 12:55:15 +0900

    Fix

--- agc040/a/a_3.cpp
+++ agc040/a/a_3.cpp
@@ -71,9 +71,7 @@ int main(int argc, char** argv) {
       for (ll j = 1; j <= x.second; ++j) {
         a[i+j] = x.second - j;
       }
-      if (i > 0 && i < N-1) {
-        a[i] = max(a[i-1], a[i+1]) + 1;
-      }
+      a[i] = max(i > 0 ? a[i-1] : 0, i < N-1 ? a[i+1] : 0) + 1;
       i += x.second;
     } else { // <
       a[i] = 0;
