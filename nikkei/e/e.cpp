f8ef45b44a098a97702f4444bb0252f673be31ee
Nao Minami <south37777@gmail.com>
2019-11-11 00:01:59 +0900

    Add nikkei/e

--- nikkei/e/e.cpp
+++ nikkei/e/e.cpp
@@ -1,3 +1,5 @@
+// base. https://atcoder.jp/contests/nikkei2019-2-qual/submissions/8355309
+
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -54,6 +56,33 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n, k;
+  cin >> n >> k;
+
+  vector< vector<ll> > a(n, vector<ll>(3));
+  rep(i, n) {
+    a[i][2] = k + 3*n - 1 - i; // a[i][2] is sorted in decreasing order.
+    if (2*i < n) {
+      a[i][0] = k + n/2 + i;
+      a[i][1] = k + 2*n - 1 - 2 * i;
+      // Here, a[i][0] + a[i][1] = 2*k + (2+1/2)*n - 1 - i. i <= n/2.
+    } else {
+      int p = (2 * i - n) / 2;
+      a[i][0] = k + p;
+      a[i][1] = k + 2*n - 2 - 2*p;
+      // Here, a[i][0] + a[i][1] = 2*k + 2*n - 2 - p
+      //                         = 2*k + (2+1/2)*n - 1 - i.
+    }
+  }
+
+  rep(i, n) {
+    if (a[i][0] + a[i][1] > a[i][2]) {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+
+  rep(i, n){
+    cout << a[i][0] << " " << a[i][1] << " " << a[i][2] << endl;
+  }
 }
