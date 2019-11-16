9ae24d492ea16ddb16b06a2142f095e380978501
Nao Minami <south37777@gmail.com>
2019-11-16 21:40:43 +0900

    Update

--- abc145/c/c.cpp
+++ abc145/c/c.cpp
@@ -48,12 +48,87 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+vector< vector<double >> dist; // Tree which contains the distance of i-j.
+
+// ll counter = 0; // For Debug
+
+// dfs returns the distance
+vector<double> dfs(set<int>& rest, int prev) {
+  // if (rest.size() == 0) { ++counter; } // FOr Debug
+  vector<double> res;
+  for (auto v : rest) {
+    set<int> ns = rest; // copy
+    ns.erase(v);
+    double d = dist[prev][v];
+
+    if (ns.size() == 0) {
+      res.emplace_back(d);
+    } else {
+      vector<double> after = dfs(ns, v); // after contains the distance of after this.
+      for (auto a_d : after) {
+        res.emplace_back(a_d + d);
+      }
+    }
+  }
+  return res;
+}
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
-  //cout << setprecision(10) << fixed;
+  cout << setprecision(10) << fixed;
 
   ll n;
   cin >> n;
+  vector<double> x(n);
+  vector<double> y(n);
+  rep(i, n) {
+    cin >> x[i];
+    cin >> y[i];
+  }
+  // printvec(x);
+  // printvec(y);
+
+  dist.assign(n, vector<double>(n)); // Tree which contains the distance of i-j.
+  rep(i, n) {
+    for (int j = i + 1; j < n; ++j) {
+      double d = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+      dist[i][j] = d;
+      dist[j][i] = d;
+      // cout << "i: " << i << endl;
+      // cout << "j: " << j << endl;
+      // cout << "d: " << d << endl;
+    }
+  }
+  // rep(i, n) {
+  //   printvec(dist[i]);
+  // }
+
+  // Now, we have distance matrix
+
+  // First, calculate the summention of all.
+  set<int> s;
+  rep(i, n) {
+    s.insert(i);
+  }
+  // Now, start set has all case.
+  double ans = 0;
+  rep(i, n) {
+    set<int> ns = s; // copy
+    ns.erase(i);
+    vector<double> res = dfs(ns, i);
+    for (auto d : res) {
+      ans += d;
+    }
+  }
+
+  ll divisor = 1;
+  rep(i, n) {
+    divisor *= i + 1;
+  }
+  // cout << "divisor: " << divisor << endl;
+  // cout << "coutner: " << counter << endl;
+
+  cout << (ans / (double)divisor) << endl;
 }
---
+++ abc145/c/c.rb
@@ -0,0 +1,10 @@
+N = gets.chomp.to_i
+# A, B, C = gets.chomp.split.map(&:to_i)
+# S = gets.chomp.chars
+X = []
+Y = []
+N.times do |i|
+  x, y = gets.chomp.split.map(&:to_i)
+  Di]
+end
+D.
