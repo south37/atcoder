02b6c170442ec32f2163c03247509a6d87ae86bb
Nao Minami <south37777@gmail.com>
2020-02-23 00:22:28 +0900

    Add abc156/e

--- abc156/e/e.cpp
+++ abc156/e/e.cpp
@@ -48,12 +48,112 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// Combination mod prime.
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct Combination {
+  vector<mint> fact, ifact;
+  Combination(int n) { init(n); }
+  void init(int n) {
+    assert(n < MOD); // n must be lower than MOD.
+
+    fact.resize(n + 1);
+    ifact.resize(n + 1);
+
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) { fact[i] = fact[i-1] * i; }
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) { ifact[i-1] = ifact[i] * i; }
+  }
+  mint operator() (int n, int k) const {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[k] * ifact[n-k];
+  }
+  mint perm(int n, int k) const {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[n-k];
+  }
+};
+
+// int main(int argc, char** argv) {
+//   MOD = 13;
+//
+//   Combination c(12);
+//   cout << c(12, 0).x << endl;  // 1  = 1 % 13
+//   cout << c(12, 1).x << endl;  // 12 = 12 % 13
+//   cout << c(12, 2).x << endl;  // 1  = 66 % 13 = (12 * 11 / 2) % 13
+//   cout << c(12, 3).x << endl;  // 12 = 220 % 13 = (12 * 11 * 10 / (3 * 2 * 1)) % 13
+//   cout << c(12, 11).x << endl; // 12 = 12 % 13
+//   cout << c(12, 12).x << endl; // 1  = 12 % 13
+//
+//   cout << c.perm(12, 3).x << endl; // 7 = 12 * 11 * 10 % 13
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n, k;
+  cin >> n >> k;
+
+  Combination c(n);
+
+  mint ans(0);
+  for (int i = 0; i <= min(n-1, k); ++i) {
+    // When the count of 0 is i, its contribution is nCi * (n-i)Hi. H is the count of combination with repetition.
+    // It is same with nCi * (n-1)C(n-i-1) because xHy = (x+y-1)C(x-1)
+    ans += c(n, i) * c(n-1, n-i-1);
+  }
+  cout << ans.x << endl;
 }
