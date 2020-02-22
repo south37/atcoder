6d10c0e48e1604b8f74a0239fc5e6d247af7a0ff
Nao Minami <south37777@gmail.com>
2020-02-22 19:00:38 +0900

    Use powmod

--- abc152/e/e.cpp
+++ abc152/e/e.cpp
@@ -52,72 +52,17 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-// Mod int
-// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
-struct mint {
-  ll x;
-  mint(ll x = 0) : x((x + MOD) % MOD) {}
-  mint& operator+= (const mint a) {
-    if ((x += a.x) >= MOD) x %= MOD;
-    return *this;
+ll powmod(ll x, ll n) { // like pow(x, n)
+  ll r = 1;
+  while (n) {
+    if (n & 1) {
+      r = r * x % MOD;
+    }
+    x = x * x % MOD;
+    n >>= 1;
   }
-  mint operator+ (const mint a) const {
-    mint res(*this);
-    return res += a;
-  }
-  mint& operator-= (const mint a) {
-    if ((x += MOD - a.x) >= MOD) x %= MOD;
-    return *this;
-  }
-  mint operator- (const mint a) const {
-    mint res(*this);
-    return res -= a;
-  }
-  mint& operator*= (const mint a) {
-    (x *= a.x) %= MOD;
-    return *this;
-  }
-  mint operator* (const mint a) const {
-    mint res(*this);
-    return res *= a;
-  }
-  mint pow(ll t) const {
-    if (!t) { return 1; }
-    mint a = pow(t >> 1);
-    a *= a;
-    if (t & 1) a *= *this;
-    return a;
-  }
-
-  // for prime mod
-  mint inv() const {
-    return pow(MOD-2);
-  }
-  mint& operator/= (const mint a) {
-    return (*this) *= a.inv();
-  }
-  mint operator/ (const mint a) const {
-    mint res(*this);
-    return res /= a;
-  }
-};
-
-// int main(int argc, char** argv) {
-//   // int p;
-//   // cin >> p;
-//
-//   MOD = 13;
-//   mint p(10);
-//   cout << (p + 15).x << endl;   // 12 (25 % 13)
-//   cout << (p - 15).x << endl;   // 8  (-5 % 13)
-//   cout << (p * 2).x << endl;    // 7  (20 % 13)
-//   cout << (p.pow(3)).x << endl; // 12 (1000 % 13)
-//   cout << (p / 3).x << endl;    // 12 (12 * 3 = 10 (36 % 13))
-//
-//   mint p2(-3);
-//   cout << p2.x << endl; // 10 (-3 % 13)
-// }
-
+  return r;
+}
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -175,8 +120,7 @@ int main(int argc, char** argv) {
       // cout << "B cnt; " << cnt << endl;
       // we should multiply by pow(factor, cnt)
       if (cnt > 0) { // exist
-        mint contrib(factor);
-        B *= contrib.pow(cnt).x;
+        B *= powmod(factor, cnt);
         B %= MOD;
       }
     }
