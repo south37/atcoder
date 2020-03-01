2661d9fd0b0620431aee18fb9c3c7809ebec45c0
Nao Minami <south37777@gmail.com>
2020-03-01 21:54:06 +0900

    Update

--- abc157/d/d.cpp
+++ abc157/d/d.cpp
@@ -48,12 +48,115 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+class UnionFind {
+public:
+  UnionFind(ll n) : par(n, -1), rnk(n, 0), cnt(n, 1), _size(n) {}
+
+  bool same(ll x, ll y) {
+    return root(x) == root(y);
+  }
+  void unite(ll x, ll y) {
+    x = root(x); y = root(y);
+    if (x == y) return;
+
+    --_size;
+
+    if (rnk[x] < rnk[y]) { swap(x, y); }
+    par[y] = x;
+    cnt[x] += cnt[y];
+    if (rnk[x] == rnk[y]) { ++rnk[x]; }
+  }
+  ll root(ll x) {
+    if (par[x] < 0) {
+      return x;
+    } else {
+      return par[x] = root(par[x]);
+    }
+  }
+  ll count(ll x) {
+    return cnt[root(x)];
+  }
+  ll size() {
+    return _size;
+  }
+
+private:
+  vector<ll> par;
+  vector<ll> rnk;
+  vector<ll> cnt; // The number of vertices in each connected components.
+  ll _size; // The number of connected components. Decreases by unite.
+};
+
+// int main(int argc, char** argv) {
+//   ll N, M;
+//   cin >> N >> M;
+//   UnionFind tree(N);
+//   rep(i, M) {
+//     ll p, a, b;
+//     cin >> p >> a >> b;
+//     if (p == 0) { // Connect
+//       tree.unite(a, b);
+//     } else { // Judge
+//       if (tree.same(a, b)) {
+//         cout << "Yes" << endl;
+//         cout << "size: " << tree.size() << endl;
+//         cout << "count(" << a << "): " << tree.count(a) << endl;
+//         cout << "count(" << b << "): " << tree.count(b) << endl;
+//       } else {
+//         cout << "No" << endl;
+//         cout << "size: " << tree.size() << endl;
+//         cout << "count(" << a << "): " << tree.count(a) << endl;
+//         cout << "count(" << b << "): " << tree.count(b) << endl;
+//       }
+//     }
+//   }
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector<vector<ll>> g(n);
+  UnionFind uf(n);
+  // scan friends
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    --a; --b; // 0-indexed
+    uf.unite(a, b);
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+  // Here, uf is calculated
+  vector<ll> cnts(n);
+  rep(i, n) {
+    cnts[i] = uf.count(i) - 1;
+  }
+  // scan block
+  rep(i, k) {
+    ll c, d;
+    cin >> c >> d;
+    --c; --d; // 0-indexed
+    if (uf.same(c, d)) { // connected
+      --cnts[c];
+      --cnts[d];
+    }
+  }
+
+  // scan connected size
+  rep(i, n) {
+    cnts[i] -= g[i].size();
+  }
+
+  rep(i, n) {
+    cout << cnts[i];
+    if (i != n-1) {
+      cout << " ";
+    }
+  }
+  cout << endl;
 }
--- abc157/e/e.cpp
+++ abc157/e/e.cpp
@@ -48,6 +48,74 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// Segment Tree
+// template <typename T>
+class SegTree {
+public:
+  SegTree(int _n, string& s) {
+    n = 1;
+    while (n < _n) { n *= 2; }
+    dat = vector<set<char>>(2 * n - 1);
+    // Initialize dat by s
+    rep(i, s.size()) {
+      update(i, s[i]);
+    }
+  }
+
+  void update(int k, char a) {
+    k += n - 1;
+    dat[k] = { a };
+    while (k > 0) {
+      k = (k - 1) / 2;
+      set<char>& l = dat[k * 2 + 1];
+      set<char>& r = dat[k * 2 + 2];
+      // set union of l and r
+      dat[k] = l; // copy
+      for (char c : r) { dat[k].insert(c); }
+    }
+  }
+
+  // Calculate the min of [a, b)
+  set<char> query(int a, int b) {
+    return _query(a, b, 0, 0, n);
+  }
+
+  // Calculate the min of [a, b)
+  // k is the index (dat[k]). This is matched to [l, r)
+  set<char> _query(int a, int b, int k, int l, int r) {
+    // The intersection of [a, b) and [r, l) is blank.
+    if (r <= a || b <= l) { return {}; }
+
+    if (a <= l && r <= b) {  // [r, l) is completely included in [a, b)
+      return dat[k];
+    } else {
+      set<char> vl = _query(a, b, k * 2 + 1, l, (l + r) / 2);
+      set<char> vr = _query(a, b, k * 2 + 2, (l + r) / 2, r);
+      // return the union of vl, vr
+      for (char c : vr) { vl.insert(c); }
+      return vl;
+    }
+  }
+
+private:
+  int n; // The size of source data. The power of 2.
+  vector<set<char>> dat; // The data. The size if 2*n-1. The last n elements(dat[n..2*n-2]) are leaves(source data). The first n-1 elements are nodes.
+};
+
+// int main(int argc, char** argv) {
+//   int arr[] = { 1, 3, 2, 7, 9, 11 };
+//   int n = 6;
+//
+//   SegTree<int> st(n);
+//   rep(i, n) {
+//     st.update(i, arr[i]);
+//   }
+//
+//   cout << st.query(1, 5) << endl;
+//   cout << st.query(0, 4) << endl;
+//   cout << st.query(3, 5) << endl;
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
@@ -56,4 +124,24 @@ int main(int argc, char** argv) {
 
   ll n;
   cin >> n;
+  string s;
+  cin >> s;
+  ll q;
+  cin >> q;
+  SegTree st(n, s);
+  rep(i, q) {
+    ll t;
+    cin >> t;
+    if (t == 1) { // update
+      ll i;
+      char c;
+      cin >> i >> c;
+      st.update(i, c);
+    } else { // query. t == 2
+      ll l, r;
+      cin >> l >> r;
+      --l; --r; // 0-indexed
+      cout << st.query(l, r+1).size() << endl;;
+    }
+  }
 }
