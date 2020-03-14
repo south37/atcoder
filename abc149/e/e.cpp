f0867fe372fd4169dbb4ed30811655c9a8d3c173
Nao Minami <south37777@gmail.com>
2020-03-14 12:54:05 +0900

    Add e

--- abc149/e/e.cpp
+++ abc149/e/e.cpp
@@ -48,12 +48,61 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+ll n;
+vector<ll> a;
+vector<ll> s; // s[i] .. the sum of a in [0, i)
+
+// return the pair of <sum, count>
+pair<ll, ll> calc(ll x) {
+  ll sum = 0, cnt = 0;
+  rep(i, n) {
+    // x-a[i] is the pair of a[i]. we want to find the boundary.
+    ll j = lower_bound(all(a), x-a[i]) - a.begin();;
+    cnt += n-j; // the count of [j, n)
+    sum += s[n] - s[j];
+    sum += a[i] * (n-j);
+  }
+  return mp(sum, cnt);
+}
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll m;
+  cin >> n >> m;
+  a.resize(n);
+  s.resize(n+1);
+
+  rep(i, n) {
+    cin >> a[i];
+  }
+  sort(all(a));
+  // printvec(a);
+
+  rep(i, n) {
+    s[i+1] = s[i] + a[i];
+  }
+  // printvec(s);
+
+  ll l = 0;
+  ll r = 2e5 + 5;
+  // find the boundary in [l, r).
+  // The count is equal or larger than m, [t, ..., t, f, ..., f]
+  while (r - l > 1) {
+    ll mid = (l+r)/2;
+    if (calc(mid).second >= m) { // ok
+      l = mid;
+    } else {
+      r = mid;
+    }
+  }
+  // Here, l is the largest true value.
+
+  auto p = calc(l);
+  ll ans = p.first;
+  ans -= (p.second - m)*l; // We want to know the sum of exact m values.
+  cout << ans << endl;
 }
