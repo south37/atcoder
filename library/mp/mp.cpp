3c475f17b8cc30fbdbef2f2555d7146022b5c7d9
Nao Minami <south37777@gmail.com>
2020-03-19 01:15:07 +0900

    Add mp.cpp

---
+++ library/mp/mp.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+// Morris-Pratt (MP, KMP)
+// https://youtu.be/9MphwmIsO7Q?t=7283
+template<typename T>
+struct MP {
+  int n;
+  T t;
+  vector<int> a;
+  MP() {}
+  MP(const T& t): t(t) {
+    n = t.size();
+    a = vector<int>(n+1);
+    a[0] = -1;
+    int j = -1;
+    for (int i = 0; i < n; ++i) {
+      while (j != -1 && t[j] != t[i]) j = a[j];
+      j++;
+      a[i+1] = j;
+    }
+  }
+  int operator[](int i) { return a[i];}
+  vector<int> findAll(const T& s) {
+    vector<int> res;
+    int j = 0;
+    for (int i = 0; i < s.size(); ++i) {
+      while (j != -1 && t[j] != s[i]) j = a[j];
+      j++;
+      if (j == n) {
+        res.push_back(i-j+1);
+        j = a[j];
+      }
+    }
+    return res;
+  }
+};
+
+int main(int argc, char** argv) {
+  string t, s;
+  cin >> t;
+  cin >> s;
+  MP<string> mp(t);
+  vector<int> ans = mp.findAll(s);
+  for (int i : ans) {
+    cout << i << endl;
+  }
+}
