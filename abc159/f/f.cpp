a0a55c33b160da656ba1e13cafec8855adc54bc8
Nao Minami <south37777@gmail.com>
2020-03-22 22:03:14 +0900

    Update

--- abc159/f/f.cpp
+++ abc159/f/f.cpp
@@ -46,7 +46,76 @@ typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
 const ll INF = 1e9;
-const ll MOD = 1000000007;  // 1e9 + 7
+const ll MOD = 998244353; // 1000000007;  // 1e9 + 7
+
+const ll MAX_V = 3010;
+
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// int main(int argc, char** argv) {
+//   // int p;
+//   // cin >> p;
+//
+//   MOD = 13;
+//   mint p(10);
+//   cout << (p + 15).x << endl;   // 12 (25 % 13)
+//   cout << (p - 15).x << endl;   // 8  (-5 % 13)
+//   cout << (p * 2).x << endl;    // 7  (20 % 13)
+//   cout << (p.pow(3)).x << endl; // 12 (1000 % 13)
+//   cout << (p / 3).x << endl;    // 12 (12 * 3 = 10 (36 % 13))
+//
+//   mint p2(-3);
+//   cout << p2.x << endl; // 10 (-3 % 13)
+// }
+
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -54,6 +123,33 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n, s;
+  cin >> n >> s;
+  vector<ll> a(n);
+  rep(i, n) {
+    cin >> a[i];
+  }
+
+
+  vector<vector<mint>> dp(MAX_V, vector<mint>(MAX_V, 0));
+  dp[0][0] = 1; // the count of [0, 0)
+
+  rep(i, n) {
+    rep(k, s+1) {
+      dp[i+1][k] = dp[i][k];
+      if (k >= a[i]) {
+        dp[i+1][k] += dp[i][k-a[i]];
+      }
+    }
+  }
+
+  // Here, dp is calculated. f(l,r) = dp[r+1][s] - dp[l][s].
+  mint ans = 0;
+  rep(l, n) {
+    for (ll r = l; r < n; ++r) {
+      ans += dp[r+1][s] - dp[l][s];
+    }
+  }
+
+  cout << ans.x << endl;
 }
