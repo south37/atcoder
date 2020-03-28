e883ba3d3cdb1e29f3105a3aa1946bf8cc9cebf9
Nao Minami <south37777@gmail.com>
2020-03-28 19:56:36 +0900

    Add abc153/e

---
+++ abc153/e/e.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e12;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll h, n;
+  cin >> h >> n;
+  vector<P> p(n);
+  rep(i, n) {
+    cin >> p[i].first >> p[i].second;
+  }
+
+  vector<ll> dp(h+1, INF); // dp[i] .. minimum cost to decrease i.
+  dp[0] = 0; // cost is 0 to decrease 0
+
+  rep(i, h) {
+    rep(j, n) {
+      ll a = p[j].first;
+      ll b = p[j].second;
+      // Here, add (a,b) to dp[i]
+
+      ll ni = i + a;
+      if (ni > h) { ni = h; }
+      dp[ni] = min(dp[ni], dp[i] + b);
+    }
+  }
+  cout << dp[h] << endl;
+}
