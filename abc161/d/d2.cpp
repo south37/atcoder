fc594dad8e005e2db48f369a8fe6e79d7d2c2c5c
Nao Minami <south37777@gmail.com>
2020-04-04 23:42:49 +0900

    Add d2.cpp

---
+++ abc161/d/d2.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll k;
+  cin >> k;
+  queue<ll> q;
+  for (int i = 1; i <= 9; ++i) {
+    q.push(i);
+  }
+
+  rep(i, k) {
+    ll x = q.front(); q.pop();
+    if (i == k-1) {
+      cout << x << endl;
+      return 0;
+    }
+
+    ll y = x * 10 + (x % 10);
+    if (x % 10 != 0) { // e.g. 10 -> 100, 101
+      q.push(y-1);
+    }
+    q.push(y);
+    if (x % 10 != 9) { // e.g. 9 -> 98, 99
+      q.push(y+1);
+    }
+  }
+}
