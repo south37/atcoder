cd5f0b66768944b6b7b55e889a8c0e4ccbaa115a
Nao Minami <south37777@gmail.com>
2020-04-11 09:25:27 +0900

    Add jsc2019-qual/d

---
+++ jsc2019-qual/d/d.cpp
@@ -0,0 +1,77 @@
+// ref. https://atcoder.jp/contests/jsc2019-qual/submissions/7110919
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n;
+  cin >> n;
+  rep(i, n-1) {
+    for (int j = i + 1; j < n; ++j) {
+      // Here, we calculate the level between i and j
+      int x = i^j; // xor. assert(x > 0)
+      int cnt = 0; // lowest significant bit.
+      while (x > 0) {
+        x >>= 1;
+        ++cnt;
+      }
+      cout << cnt;
+      if (j < n-1) {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
