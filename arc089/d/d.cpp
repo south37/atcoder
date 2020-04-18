4b89630d9a724e39717ef04d066085ae226223b1
Nao Minami <south37777@gmail.com>
2020-04-19 00:07:46 +0900

    Update

--- arc089/d/d.cpp
+++ arc089/d/d.cpp
@@ -1,3 +1,5 @@
+// ref. https://atcoder.jp/contests/arc089/submissions/2001908
+
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -48,105 +50,61 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+ll n, K;
+vector<vector<ll>> mat;
+
+// return the sum of [x, x2), [y, y2)
+ll getSum(ll x, ll y, ll x2, ll y2) {
+  return mat[x2][y2] + mat[x][y] - mat[x2][y] - mat[x][y2];
+}
+
+// (x,y) .. point of left-bottom
+ll solve(ll x, ll y) {
+  if (x <= K) {
+    // left -> right
+    return getSum(0, 0, x, y) + getSum(x, y, x+K, K) + getSum(x+K, 0, 2*K, y);
+  } else {
+    // right -> left
+    return getSum(x, y, 2*K, K) + getSum(x-K, 0, x, y) + getSum(0, y, x-K, K);
+  }
+}
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n, K;
   cin >> n >> K;
-  vector<vector<vector<ll>>> mats(2, vector<vector<ll>>(K, vector<ll>(K)));
-  auto& black = mats[0]; // counts only black.
-  auto& white = mats[1]; // counts only white.
+  mat.assign(2*K+1, vector<ll>(K+1));
   rep(i, n) {
     ll x, y;
     char c;
     cin >> x >> y >> c;
-    int color = (c == 'B') ? 1 : 0; // 1 .. black, 0 .. white
+    if (c == 'W') {
+      y += K; // covert to black
+    }
     x %= 2*K;
     y %= 2*K;
-    if (x >= K) {
-      x -= K;
-      color ^= 1;
-    }
     if (y >= K) {
+      x += K;
       y -= K;
-      color ^= 1;
     }
-
-    if (color == 1) { // black
-      ++black[x][y];
-    } else { // white
-      ++white[x][y];
-    }
+    x %= 2*K;
+    // Here, y < K
+    ++mat[x+1][y+1];
   }
-  // printtree(black);
-  // printtree(white);
+  // mat[x+1][y+1] .. the count of black at (x, y)
 
-  vector<ll> sums(2);
-  vector<vector<vector<ll>>> lefts(2, vector<vector<ll>>(K, vector<ll>(K))); // sum of bottom-left
-  vector<vector<vector<ll>>> rights(2, vector<vector<ll>>(K, vector<ll>(K))); // sum of top-right
-  rep(k, 2) {
-    auto& mat = mats[k];
-
-    auto& sum = sums[k];
-    rep(x, K) rep(y, K) {
-      sum += mat[x][y];
-    }
-
-    auto& left = lefts[k];
-    auto& right = rights[k];
-
-    rep(x, K) rep(y, K) {
-      if (x == 0 && y == 0) {
-        left[x][y] = mat[x][y];
-      } else if (x == 0) {
-        left[x][y] = left[x][y-1] + mat[x][y];
-      } else if (y == 0) {
-        left[x][y] = left[x-1][y] + mat[x][y];
-      } else { // x > 0 && y > 0
-        left[x][y] = left[x][y-1] + left[x-1][y] - left[x-1][y-1] + mat[x][y];
-      }
-    }
-
-    for (int x = K-1; x >= 0; --x) for (int y = K-1; y >= 0; --y) {
-      if (x == K-1 && y == K-1) {
-        right[x][y] = mat[x][y];
-      } else if (x == K-1) {
-        right[x][y] = right[x][y+1] + mat[x][y];
-      } else if (y == K-1) {
-        right[x][y] = right[x+1][y] + mat[x][y];
-      } else { // x < K-1 && y < K-1
-        right[x][y] = right[x][y+1] + right[x+1][y] - right[x+1][y+1] + mat[x][y];
-      }
-    }
+  // Here, we calculate the cummulative sum
+  rep(x, 2*K) rep(y, K) {
+    mat[x+1][y+1] += mat[x][y+1] + mat[x+1][y] - mat[x][y];
   }
-  // printtree(lefts[0]);
-  // printtree(rights[0]);
-  // printtree(lefts[1]);
-  // printtree(rights[1]);
+  // mat[x][y] .. the cummulative sum of [0, x), [0, y) in x<K, y<K
 
-  // k=0 .. black, k=1 .. white
-  ll white_sum = sums[1];
-  auto& white_left = lefts[1];
-  auto& white_right = rights[1];
-  auto& black_left = lefts[0];
-  auto& black_right = rights[0];
-
-  ll ans = max(sums[0], sums[1]);
-  // check all case of boundary
-  rep(x, K+1) rep(y, K+1) {
-    ll now = white_sum;
-    if (x > 0 && y > 0) {
-      now -= white_left[x-1][y-1];
-      now += black_left[x-1][y-1];
-    }
-    if (x < K && y < K) {
-      now -= white_right[x][y];
-      now += black_right[x][y];
-    }
-    ans = max(ans, now);
+  ll ans = 0;
+  rep(x, 2*K) rep(y, K) {
+    ans = max(ans, solve(x, y));
   }
   cout << ans << endl;
 }
