e9a213cbb0489563bb2152f2f5fcf11494e61573
Nao Minami <south37777@gmail.com>
2020-04-26 19:48:23 +0900

    Add edu_dp/k

---
+++ edu_dp/k/k.cpp
@@ -0,0 +1,85 @@
+// ref. https://juppy.hatenablog.com/entry/2019/03/18/Educational_DP_Contest%28EDPC%29_G~K_Python_%E7%AB%B6%E6%8A%80%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0_Atcoder#EDPC--K
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> a(n);
+  rep(i, n) {
+    cin >> a[i];
+  }
+  sort(all(a));
+
+  vector<bool> dp(k+1); // dp[i] .. first can win when remaining is i.
+  dp[0] = false;
+  rep(i, k+1) {
+    rep(j, n) {
+      if (i-a[j] >= 0) {
+        dp[i] = (dp[i] | !dp[i-a[j]]);
+      }
+    }
+  }
+  if (dp[k]) {
+    cout << "First" << endl;
+  } else {
+    cout << "Second" << endl;
+  }
+}
