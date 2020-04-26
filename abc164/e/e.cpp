a92c74ff3bb85c25b12bb3ab338b83fae664fefd
Nao Minami <south37777@gmail.com>
2020-04-26 22:06:39 +0900

    Update

--- abc164/e/e.cpp
+++ abc164/e/e.cpp
@@ -49,8 +49,28 @@ typedef double D;
 typedef vector<ll> vl;
 typedef vector<P> vp;
 
-const ll INF = 1e9;
+const ll INF = 1e15;
 const ll MOD = 1000000007;  // 1e9 + 7
+
+const ll MAX_A = 55;
+const ll MAX_N = 55;
+
+vector<ll> g[MAX_N];
+vector<vector<ll>> dp;
+ll cost[MAX_A][MAX_A];
+ll t[MAX_A][MAX_A];
+ll n, m;
+
+ll rec(ll v, ll j) {
+  if (dp[v][j] != INF) { return dp[v][j]; }
+  for (int nv : g[v]) {
+    ll precost = j - cost[nv][v];
+    if (precost >= 0 && precost <= MAX_A*MAX_N) { // Here, consume a[u][v]
+      chmin(dp[v][j], rec(nv, precost) + t[nv][v]);
+    }
+  }
+  return dp[v][j];
+}
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -58,6 +78,35 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll s;
+  cin >> n >> m >> s;
+  rep(i, m) {
+    ll u, v, a, b;
+    cin >> u >> v >> a >> b;
+    --u; --v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+    cost[u][v] = a;
+    cost[v][u] = a;
+    t[u][v] = b;
+    t[v][u] = b;
+  }
+  rep(i, n) {
+    g[i].push_back(i); // add self
+  }
+  rep(i, n) {
+    ll a, b;
+    cin >> a >> b;
+    cost[i][i] = -a;
+    cost[i][i] = -a;
+    t[i][i] = b;
+    t[i][i] = b;
+  }
+
+  for (ll t = 1; t < n; ++t) {
+    dp.assign(n, vector<ll>(MAX_A * MAX_N + 5, INF));
+    dp[0][0] = 0; // start from 0
+    rec(t, 0);
+    cout << dp[t][0] << endl;
+  }
 }
