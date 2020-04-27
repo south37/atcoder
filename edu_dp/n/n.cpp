2025860cfae283aa7dc246eec3d570a8a37a1977
Nao Minami <south37777@gmail.com>
2020-04-28 00:11:40 +0900

    Add edu_dp/n

---
+++ edu_dp/n/n.cpp
@@ -0,0 +1,96 @@
+// ref. https://juppy.hatenablog.com/entry/2019/03/19/Educational_DP_Contest%28EDPC%29_M~P_Python_%E7%AB%B6%E6%8A%80%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0_Atcoder
+// ref. https://atcoder.jp/contests/dp/submissions/3936122
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e15;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+const ll N = 405;
+// dp[i][j] .. min cost of [i, j]
+ll dp[N][N];
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, n) {
+    cin >> a[i];
+  }
+  vector<ll> s(n+1);
+  rep(i, n) {
+    s[i+1] = s[i] + a[i];
+  }
+
+  rep(i, n) rep(j, n) {
+    dp[i][j] = INF;
+  }
+  rep(i, n) {
+    dp[i][i] = 0;
+  }
+  for (ll d = 2; d <= n; ++d) { // distance. d=r-l+1
+    rep(l, n) {
+      ll r = l+d-1;
+      if (r >= n) { continue; } // invalid
+      for (ll k = l; k < r; ++k) {
+        chmin(dp[l][r], dp[l][k] + dp[k+1][r]);
+      }
+      dp[l][r] += s[r+1] - s[l]; // sum of [l, r]
+    }
+  }
+  cout << dp[0][n-1] << endl;
+}
