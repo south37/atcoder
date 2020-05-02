0eff32a6b8dc957f11782da2fbfb1859df176c25
Nao Minami <south37777@gmail.com>
2020-05-02 23:05:35 +0900

    Update c

--- abc165/c/c.cpp
+++ abc165/c/c.cpp
@@ -52,52 +52,49 @@ typedef vector<P> vp;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+ll ans;
+ll n, m, q;
+vector<tuple<ll,ll,ll,ll>> queries;
+
+void backtrack(vector<ll>& now, ll i) {
+  if (i == n) {
+    ll score = 0;
+    for (auto& query : queries) {
+      ll a, b, c, d;
+      tie(a, b, c, d) = query;
+      if (now[b] - now[a] == c) {
+        score += d;
+      }
+    }
+    chmax(ans, score);
+    return;
+  }
+
+  ll pre = now[i]; // prev value
+  for (ll v = pre; v <= m; ++v) { // scan value in [pre, m]
+    now.push_back(v);
+    backtrack(now, i+1);
+    now.pop_back();
+  }
+}
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n, m, q;
   cin >> n >> m >> q;
-  // dp[i][j] .. using[0,i), last value is j.
-  vector<vector<ll>> dp(n+1, vector<ll>(m+1));
-  vector<tuple<ll,ll,ll,ll>> queries;
 
-  map<ll, vector<tuple<ll,ll,ll>>> mp;
   rep(iter,q) {
     ll a, b, c, d;
     cin >> a >> b >> c >> d;
     --a; --b; // 0-indexed
-    // queries.push_back(tuple<ll,ll,ll,ll>(b,a,c,d));
-    mp[b].push_back(tuple<ll,ll,ll>(a,c,d));
+    queries.push_back(tuple<ll,ll,ll,ll>(a,b,c,d));
   }
-  // sort(all(queries));
-  // Here, queries are sorted by b.
 
-  rep(i,n) {
-    if (mp.find(i) != mp.end()) { // query exist
-      vector<tuple<ll,ll,ll>>& p = mp[i];
-      sort(all(p)); // sort by a.
-      for (ll j = 1; j <= m; ++j) {
-        for (auto& pp : p) {
-          ll a, c, d;
-          tie(a, c, d) = pp;
-          cout << "a,b,c,d:" << a<<","<<i<<","<<c<<","<<d<<endl;
-          // Here, if diff is c, add d
-          if (j+c <= m) {
-            chmax(dp[i][j+c], dp[a][j] + d);
-          }
-        }
-      }
-    }
-  }
-  printtree(dp);
-
-  // Here, max value in dp[n-1] is result.
-  ll ans = 0;
-  for (ll i = 1; i <= m; ++i) {
-    chmax(ans, dp[n-1][i]);
-  }
+  vector<ll> now;
+  now.push_back(1); // start from 1.
+  backtrack(now, 0);
   cout << ans << endl;
 }
