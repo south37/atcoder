dfab74adc5a71918376aae60c63fc68f596783b9
Nao Minami <south37777@gmail.com>
2020-05-02 17:54:32 +0900

    Add DPL/4_b

---
+++ aizu/DPL/4_b/b.cpp
@@ -0,0 +1,141 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+// BIT (Fenwick Tree) with 0-indexed query
+// https://youtu.be/lyHk98daDJo?t=7960
+template<typename T>
+struct BIT {
+  int n;
+  vector<T> d;
+  BIT(int n=0) : n(n), d(n+1) {}
+  void add(int i, T x=1) {
+    i++; // 0-indexed to 1-indexed
+    for (; i <= n; i += i&-i) {
+      d[i] += x;
+    }
+  }
+  // The sum of [0, i]
+  T sum(int i) {
+    i++; // 0-indexed to 1-indexed
+    T x = 0;
+    for (; i; i -= i&-i) {
+      x += d[i];
+    }
+    return x;
+  }
+  // The sum of [l, r)
+  T sum(int l, int r) {
+    return sum(r-1) - sum(l-1);
+  }
+};
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n, k, l, r;
+  cin >> n >> k >> l >> r;
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+  }
+
+  ll n2 = n/2;
+  ll remain = n-n2;
+  vector<vector<vector<ll>>> dp(2);
+  vector<vector<ll>>& dp1 = dp[0];
+  vector<vector<ll>>& dp2 = dp[1];
+  dp1.assign(n2+1, vector<ll>(1ll<<n2)); // prev
+  dp2.assign(remain+1, vector<ll>(1ll<<remain)); // latter
+
+  rep(k,2) {
+    auto& d = dp[k];
+    ll m = d[0].size();
+
+    rep(i,1ll<<m) {
+      ll v = 0; // sum of value
+      ll cnt = 0; // count
+      rep(j,m) {
+        if (i&(1ll<<j)) {
+          v += a[j];
+          ++cnt;
+        }
+      }
+      d[cnt].push_back(v);
+    }
+    reverse(all(a));
+  }
+  rep(i,n2) {
+    sort(all(dp1[i]));
+  }
+
+  ll ans = 0;
+  // Try all i in latter
+  rep(i, k+1) {
+    if (i+n2 < k) { continue; } // too small
+    if (i > remain) { continue; } // too large
+    // Here, consider i and k-i.
+    for (ll v : dp2[i]) {
+      // Here, l <= lv+rv <= r
+      // <=> l-rv <= lv <= r-rv
+      auto rit = upper_bound(all(dp1[k-i]), r-v);
+      auto lit = lower_bound(all(dp1[k-i]), l-v);
+      // result is count of [lit, rit)
+      ans += rit-lit;
+    }
+  }
+  cout << ans << endl;
+}
