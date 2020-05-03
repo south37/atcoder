b4b86a5f7919aabdf3571330983e0773220d9867
Nao Minami <south37777@gmail.com>
2020-05-03 18:56:52 +0900

    Update

--- library/BitMatrix/yukicoder184.cpp
+++ library/BitMatrix/yukicoder184.cpp
@@ -38,20 +38,52 @@ const ll MAX_COL = 100010; // To be set appropriately.
 
 class BitMatrix {
 public:
-  BitMatrix(int m = 1, int n = 1) : H(m), W(n) {}
+  BitMatrix(int h = 1, int w = 1) : H(h), W(w) {}
   inline bitset<MAX_COL>& operator [] (int i) { return val[i]; }
+  inline const bitset<MAX_COL>& operator [] (int i) const { return val[i]; }
   int H, W;
+  size_t size() const { return H; }
+  BitMatrix transpose() const {
+    BitMatrix R(W, H);
+    rep(r,H)rep(c,W) {
+      R[c][r] = val[r][c];
+    }
+    return R;
+  }
+
 private:
   bitset<MAX_COL> val[MAX_ROW];
 };
 
-ostream& operator << (ostream& s, BitMatrix A) {
+ostream& operator << (ostream& s, BitMatrix& A) {
   s << endl;
   rep(i, A.H) {
     rep(j, A.W) { s << A[i][j] << ", "; }
     s << endl;
   }
   return s;
+}
+
+BitMatrix operator * (const BitMatrix& A, const BitMatrix& B) {
+  BitMatrix revB = B.transpose();
+  BitMatrix R(A.size(), B[0].size());
+  rep(r,A.H)rep(c,B[0].size()) {
+    R[r][c] = (A[r]&revB[c]).count()&1;
+  }
+  return R;
+}
+
+BitMatrix pow(BitMatrix& A, ll n) {
+  BitMatrix R(A.size(), A.size());
+  rep(i, A.size()) {
+    R[i][i] = 1;
+  }
+  while (n > 0) {
+    if (n & 1) { R = R * A; }
+    A = A * A;
+    n >>= 1;
+  }
+  return R;
 }
 
 // GaussJordan
