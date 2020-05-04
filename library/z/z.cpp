f3293e6de2e04d00477101807ec9ff2674f5ea18
Nao Minami <south37777@gmail.com>
2020-05-04 15:55:45 +0900

    Update

--- library/z/z.cpp
+++ library/z/z.cpp
@@ -4,16 +4,17 @@
 #include <vector>
 
 using namespace std;
+typedef long long ll;
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
 
-vector<int> build_z(const string& s) {
-  int n = s.size();
-  vector<int> z(n, 0);
-  int l = -1, r = -1;
-  for (int i = 1; i < n; ++i) {
+vector<ll> build_z(const string& s) {
+  ll n = s.size();
+  vector<ll> z(n, 0);
+  ll l = -1, r = -1;
+  for (ll i = 1; i < n; ++i) {
     if (i <= r) {
-      z[i] = min<int>(r - i + 1, z[i - l]);
+      z[i] = min<ll>(r - i + 1, z[i - l]);
     }
     while (i + z[i] < n && s[i + z[i]] == s[z[i]]) {
       ++z[i];
@@ -26,11 +27,11 @@ vector<int> build_z(const string& s) {
   return z;
 }
 
-void search(const string& text, const string& pattern, vector<int>& result) {
-  vector<int> z = build_z(pattern + "$" + text);
+void search(const string& text, const string& pattern, vector<ll>& result) {
+  vector<ll> z = build_z(pattern + "$" + text);
 
-  int p = pattern.length();
-  for (int i = p + 1; i < z.size(); ++i) {
+  ll p = pattern.length();
+  for (ll i = p + 1; i < z.size(); ++i) {
     if (z[i] == p) {
       result.push_back(i-p-1);
     }
@@ -42,11 +43,11 @@ int main(int argc, char** argv) {
   cin >> s;
   cin >> t;
 
-  vector<int> matches;
+  vector<ll> matches;
   search(s, t, matches);
 
-  for (auto e : matches) {
-    cout << e << endl;
+  for (ll idx : matches) {
+    cout << idx << endl;
   }
 
   cout << s << endl;
