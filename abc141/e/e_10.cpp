248c3e5e34f48641d417865aae993e30393353b5
Nao Minami <south37777@gmail.com>
2020-05-06 19:48:31 +0900

    Add e_10

---
+++ abc141/e/e_10.cpp
@@ -0,0 +1,123 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+vector<ll> build_z(const string& s) {
+  ll n = s.size();
+  vector<ll> z(n, 0);
+  ll l = -1, r = -1;
+  for (ll i = 1; i < n; ++i) {
+    if (i <= r) {
+      z[i] = min<ll>(r - i + 1, z[i - l]);
+    }
+    while (i + z[i] < n && s[i + z[i]] == s[z[i]]) {
+      ++z[i];
+    }
+    if (i + z[i] - 1 > r) {
+      r = i + z[i] - 1;
+      l = i;
+    }
+  }
+  return z;
+}
+
+void search(const string& text, const string& pattern, vector<ll>& result) {
+  vector<ll> z = build_z(pattern + "$" + text);
+
+  ll p = pattern.length();
+  for (ll i = p + 1; i < z.size(); ++i) {
+    if (z[i] == p) {
+      result.push_back(i-p-1);
+    }
+  }
+}
+
+// int main(int argc, char** argv) {
+//   string s, t;
+//   cin >> s;
+//   cin >> t;
+//
+//   vector<ll> matches;
+//   search(s, t, matches);
+//
+//   for (ll idx : matches) {
+//     cout << idx << endl;
+//   }
+//
+//   cout << s << endl;
+//   cout << t << endl;
+// }
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n;
+  cin >> n;
+  string S;
+  cin >> S;
+
+  ll ans = 0;
+  for (ll i = 1; i < n; ++i) { // prefix..[0..i), suffix..[i,n)
+    string pre = S.substr(0,i);
+    string suf = S.substr(i);
+    string nexS = suf + "$" + pre;
+    vector<ll> z = build_z(suf+"$"+pre);
+    for (ll j = n+1-i; j < n+1; ++j) {
+      chmax(ans, z[j]);
+    }
+  }
+  cout << ans << endl;
+}
