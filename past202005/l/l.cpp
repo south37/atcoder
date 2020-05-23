c1ee4af39694d27f7145e0bfff8a5b207da00c7e
Nao Minami <south37777@gmail.com>
2020-05-23 15:54:07 +0900

    Update

--- past202005/l/l.cpp
+++ past202005/l/l.cpp
@@ -49,7 +49,7 @@ typedef double D;
 typedef vector<ll> vl;
 typedef vector<P> vp;
 
-const ll INF = 1e9;
+const ll INF = 1e12;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 int main(int argc, char** argv) {
@@ -60,4 +60,86 @@ int main(int argc, char** argv) {
 
   ll n;
   cin >> n;
+  vector<queue<ll>> qs(n); // i .. row i.
+  rep(i,n) {
+    ll k;
+    cin >> k;
+    rep(j,k) {
+      ll t;
+      cin >> t;
+      qs[i].push(t);
+    }
+  }
+
+  set<P> tops; // set of <value,idx>
+  set<P> seconds; // set of <value,idx>
+  map<ll,ll> topsMp; // { idx => value }
+  map<ll,ll> secondsMp; // { idx => value }
+
+  // pop v from qs[i] and set it to st and mp
+  auto add = [&](set<P>& st, map<ll,ll>& mp, ll i) {
+    ll v;
+    if (qs[i].empty()) {
+      v = 0;
+    } else {
+      v = qs[i].front(); qs[i].pop();
+    }
+    st.insert(P(v,i));
+    mp[i] = v;
+  };
+
+  // setup tops
+  rep(i,n) {
+    add(tops, topsMp, i);
+  }
+
+  // setup seconds
+  rep(i,n) {
+    add(seconds, secondsMp, i);
+  }
+
+  // move idx from seconds to tops
+  auto moveToTopFromSecond = [&](ll i) {
+    ll v = secondsMp[i];
+    auto it = seconds.find(P(v,i)); // pick from seconds
+    seconds.erase(it);
+
+    P p = *it; // picked from seconds
+    tops.insert(p);
+    topsMp[p.second] = p.first; // update mp
+
+    add(seconds, secondsMp, i); // from queue to second
+  };
+
+  ll m;
+  cin >> m;
+  rep(iter,m) {
+    ll a;
+    cin >> a;
+    if (a == 1) {
+      // Here, we pick only from tops
+      auto it = prev(tops.end()); // most largest
+
+      cout << it->first << endl;
+      ll idx = it->second;
+      tops.erase(it);
+      moveToTopFromSecond(idx);
+    } else { // a == 2
+      // Here, we pick from tops and seconds
+      auto itTop = prev(tops.end()); // most largest
+      auto itSecond = prev(seconds.end()); // most largest
+
+      if (itTop->first > itSecond->first) { // top is largest
+        cout << itTop->first << endl;
+        ll idx = itTop->second;
+        tops.erase(itTop);
+        moveToTopFromSecond(idx);
+      } else { // second id largest
+        cout << itSecond->first << endl;
+        ll idx = itSecond->second;
+        seconds.erase(itSecond);
+        add(seconds, secondsMp, idx); // move from queue to seconds
+      }
+    }
+  }
 }
