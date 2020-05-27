8973bb020900219143b8511e41c20c013c262cca
Nao Minami <south37777@gmail.com>
2020-05-28 02:43:25 +0900

    Add joi2009ho/b

---
+++ joi2009ho/b/b.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll d,n,m;
+  cin >> d;
+  cin >> n;
+  cin >> m;
+  vector<ll> a(n);
+  rep(i,n-1) {
+    cin >> a[i+1];
+  }
+  sort(all(a));
+  a.push_back(d); // push d as sentinel
+
+  // printvec(a);
+
+  ll ans = 0;
+  rep(iter,m) {
+    ll k;
+    cin >> k;
+    auto idx = upper_bound(all(a), k) - a.begin();
+    ll now = min(abs(a[idx]-k), abs(a[idx-1]-k));
+    ans += now;
+
+    // cout << "k:" << k << endl;
+    // cout << "idx:" << idx << endl;
+    // cout << "a[idx]:" << a[idx] << endl;
+    // cout << "a[idx-1]:" << a[idx]-1 << endl;
+    // cout << "now:" << now << endl;
+  }
+  cout << ans << endl;
+}
