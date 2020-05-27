47e563eacb4601c71dacd48ff06300b08ae98fd3
Nao Minami <south37777@gmail.com>
2020-05-28 00:43:55 +0900

    Add LowLink as library

---
+++ library/LowLink/a.cpp
@@ -0,0 +1,121 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+// LowLink
+template<typename G>
+struct LowLink {
+  const G &g;
+  vector<int> used, ord, low;
+  vector<int> articulation;
+  vector<pair<int, int>> bridge;
+
+  LowLink(const G &g) : g(g) {}
+
+  int dfs(int idx, int k, int par) {
+    used[idx] = true;
+    ord[idx] = k++;
+    low[idx] = ord[idx];
+    bool is_articulation = false;
+    int cnt = 0;
+    for(auto &to : g[idx]) {
+      if(!used[to]) {
+        ++cnt;
+        k = dfs(to, k, idx);
+        low[idx] = min(low[idx], low[to]);
+        is_articulation |= ~par && low[to] >= ord[idx];
+        if(ord[idx] < low[to]) bridge.emplace_back(minmax(idx, (int) to));
+      } else if(to != par) {
+        low[idx] = min(low[idx], ord[to]);
+      }
+    }
+    is_articulation |= par == -1 && cnt > 1;
+    if(is_articulation) articulation.push_back(idx);
+    return k;
+  }
+
+  virtual void build() {
+    used.assign(g.size(), 0);
+    ord.assign(g.size(), 0);
+    low.assign(g.size(), 0);
+    int k = 0;
+    for(int i = 0; i < g.size(); i++) {
+      if(!used[i]) k = dfs(i, k, -1);
+    }
+  }
+};
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n,m;
+  cin >> n >> m;
+  vector<vector<ll>> g(n);
+  rep(i,m) {
+    ll s,t;
+    cin >> s >> t;
+    g[s].push_back(t);
+    g[t].push_back(s);
+  }
+  LowLink<vector<vector<ll>>> t(g);
+  t.build();
+
+  vector<int> ans = t.articulation; // copy
+  sort(all(ans));
+  for (ll v : ans) {
+    cout << v << endl;
+  }
+}
