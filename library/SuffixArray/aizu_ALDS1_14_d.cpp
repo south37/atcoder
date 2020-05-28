2399ebde878535f65c1dda32c53898ca8a240449
Nao Minami <south37777@gmail.com>
2020-05-29 00:49:00 +0900

    Add library/SuffixArray

---
+++ library/SuffixArray/aizu_ALDS1_14_d.cpp
@@ -0,0 +1,190 @@
+// ref. https://ei1333.github.io/luzhiled/snippets/string/suffix-array.html
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+// SuffixArray
+struct SuffixArray {
+  vector< int > SA;
+  const string s;
+
+  SuffixArray(const string &str) : s(str) {
+    SA.resize(s.size());
+    iota(begin(SA), end(SA), 0);
+    sort(begin(SA), end(SA), [&](int a, int b) {
+      return s[a] == s[b] ? a > b : s[a] < s[b];
+    });
+    vector< int > classes(s.size()), c(s.begin(), s.end()), cnt(s.size());
+    for(int len = 1; len < s.size(); len <<= 1) {
+      for(int i = 0; i < s.size(); i++) {
+        if(i > 0 && c[SA[i - 1]] == c[SA[i]] && SA[i - 1] + len < s.size() && c[SA[i - 1] + len / 2] == c[SA[i] + len / 2]) {
+          classes[SA[i]] = classes[SA[i - 1]];
+        } else {
+          classes[SA[i]] = i;
+        }
+      }
+      iota(begin(cnt), end(cnt), 0);
+      copy(begin(SA), end(SA), begin(c));
+      for(int i = 0; i < s.size(); i++) {
+        int s1 = c[i] - len;
+        if(s1 >= 0) SA[cnt[classes[s1]]++] = s1;
+      }
+      classes.swap(c);
+    }
+  }
+
+  int operator[](int k) const {
+    return SA[k];
+  }
+
+  size_t size() const {
+    return s.size();
+  }
+
+  bool lt_substr(const string &t, int si = 0, int ti = 0) {
+    int sn = (int) s.size(), tn = (int) t.size();
+    while(si < sn && ti < tn) {
+      if(s[si] < t[ti]) return true;
+      if(s[si] > t[ti]) return false;
+      ++si, ++ti;
+    }
+    return si >= sn && ti < tn;
+  }
+
+  int lower_bound(const string &t) {
+    int low = -1, high = (int) SA.size();
+    while(high - low > 1) {
+      int mid = (low + high) / 2;
+      if(lt_substr(t, SA[mid])) low = mid;
+      else high = mid;
+    }
+    return high;
+  }
+
+  pair< int, int > lower_upper_bound(string &t) {
+    int idx = lower_bound(t);
+    int low = idx - 1, high = (int) SA.size();
+    t.back()++;
+    while(high - low > 1) {
+      int mid = (low + high) / 2;
+      if(lt_substr(t, SA[mid])) low = mid;
+      else high = mid;
+    }
+    t.back()--;
+    return {idx, high};
+  }
+
+  void output() {
+    for(int i = 0; i < size(); i++) {
+      cout << i << ": " << s.substr(SA[i]) << endl;
+    }
+  }
+};
+
+struct LongestCommonPrefixArray {
+  const SuffixArray &SA;
+  vector< int > LCP, rank;
+
+  LongestCommonPrefixArray(const SuffixArray &SA) : SA(SA), LCP(SA.size()) {
+    rank.resize(SA.size());
+    for(int i = 0; i < SA.size(); i++) {
+      rank[SA[i]] = i;
+    }
+    for(int i = 0, h = 0; i < SA.size(); i++) {
+      if(rank[i] + 1 < SA.size()) {
+        for(int j = SA[rank[i] + 1]; max(i, j) + h < SA.size() && SA.s[i + h] == SA.s[j + h]; ++h);
+        LCP[rank[i] + 1] = h;
+        if(h > 0) --h;
+      }
+    }
+  }
+
+  int operator[](int k) const {
+    return LCP[k];
+  }
+
+  size_t size() const {
+    return LCP.size();
+  }
+
+  void output() {
+    for(int i = 0; i < size(); i++) {
+      cout << i << ": " << LCP[i] << " " << SA.s.substr(SA[i]) << endl;
+    }
+  }
+};
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  string s;
+  cin >> s;
+  SuffixArray sa(s);
+  ll q;
+  cin >> q;
+  while(q--) {
+    string t;
+    cin >> t;
+    pair<int,int> p = sa.lower_upper_bound(t);
+    if (p.first != p.second) { // found
+      cout << 1 << endl;
+    } else {
+      cout << 0 << endl;
+    }
+  }
+}
