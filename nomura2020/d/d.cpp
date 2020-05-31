d26ae44ef911d7ff2c12c2d1a1d11160c4ce9510
Nao Minami <south37777@gmail.com>
2020-05-31 16:38:50 +0900

    Add nomura2020/d

--- nomura2020/d/d.cpp
+++ nomura2020/d/d.cpp
@@ -1,3 +1,6 @@
+// ref. https://www.youtube.com/watch?v=NuhtAfH5SCc&feature=youtu.be
+// ref. https://atcoder.jp/contests/nomura2020/submissions/13732813
+
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -54,6 +57,122 @@ typedef vector<P> vp;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+class UnionFind {
+public:
+  UnionFind(ll n) : par(n, -1), rnk(n, 0), cnt(n, 1), _size(n) {}
+
+  bool same(ll x, ll y) {
+    return root(x) == root(y);
+  }
+  void unite(ll x, ll y) {
+    x = root(x); y = root(y);
+    if (x == y) return;
+
+    --_size;
+
+    if (rnk[x] < rnk[y]) { swap(x, y); }
+    par[y] = x;
+    cnt[x] += cnt[y];
+    if (rnk[x] == rnk[y]) { ++rnk[x]; }
+  }
+  ll root(ll x) {
+    if (par[x] < 0) {
+      return x;
+    } else {
+      return par[x] = root(par[x]);
+    }
+  }
+  ll count(ll x) {
+    return cnt[root(x)];
+  }
+  ll size() {
+    return _size;
+  }
+
+private:
+  vector<ll> par;
+  vector<ll> rnk;
+  vector<ll> cnt; // The number of vertices in each connected components.
+  ll _size; // The number of connected components. Decreases by unite.
+};
+
+// int main(int argc, char** argv) {
+//   ll N, M;
+//   cin >> N >> M;
+//   UnionFind tree(N);
+//   rep(i, M) {
+//     ll p, a, b;
+//     cin >> p >> a >> b;
+//     if (p == 0) { // Connect
+//       tree.unite(a, b);
+//     } else { // Judge
+//       if (tree.same(a, b)) {
+//         cout << "Yes" << endl;
+//         cout << "size: " << tree.size() << endl;
+//         cout << "count(" << a << "): " << tree.count(a) << endl;
+//         cout << "count(" << b << "): " << tree.count(b) << endl;
+//       } else {
+//         cout << "No" << endl;
+//         cout << "size: " << tree.size() << endl;
+//         cout << "count(" << a << "): " << tree.count(a) << endl;
+//         cout << "count(" << b << "): " << tree.count(b) << endl;
+//       }
+//     }
+//   }
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
@@ -62,4 +181,52 @@ int main(int argc, char** argv) {
 
   ll n;
   cin >> n;
+  vector<ll> p(n);
+  UnionFind uf(n);
+  ll k = 0; // count of -1
+
+  rep(i,n) {
+    cin >> p[i];
+    if (p[i] >= 0) {
+      --p[i];
+      uf.unite(i,p[i]);
+    } else {
+      ++k;
+    }
+  }
+
+  ll gn = uf.size(); // size of connected components
+  ll already = gn - k; // size of connected components without k.
+
+  vector<ll> vs; // count of each connected components
+  rep(i,n) {
+    if (p[i] != -1) { continue; }
+    vs.push_back(uf.count(i));
+  }
+
+  // Here, we count the cycle. sum of (n-cycle) is the result.
+  mint ans = mint(n) * mint(n-1).pow(k);
+  ans -= mint(already) * mint(n-1).pow(k); // already is cycle, so decrease it .
+
+  // Next, decrease the contribution of cycles without already.
+  // dp[j] .. sum of vs[i] when total count is j (pick j elements from vs).
+  vector<mint> dp(k+1);
+  dp[0] = 1;
+  for (auto x : vs) {
+    for (int i = k-1; i >= 0; --i) { // by loop from larger value, we can re-use dp vector.
+      dp[i+1] += dp[i]*mint(x);
+    }
+  }
+
+  if (k) {
+    // each k has -1 (it can not select itself)
+    dp[1] -= mint(k);
+  }
+  mint fact = 1; // fact = (i-1)!
+  for (int i = 1; i <= k; ++i) { // i .. length of cycle
+    ans -= dp[i]*mint(n-1).pow(k-i)*fact;
+    fact *= mint(i);
+  }
+
+  cout << ans << endl;
 }
