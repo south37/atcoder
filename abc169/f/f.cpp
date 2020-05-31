ac018eaf71f7c3fe46165ef0c64f4613c892d7c9
Nao Minami <south37777@gmail.com>
2020-05-31 22:09:07 +0900

    Add f

--- abc169/f/f.cpp
+++ abc169/f/f.cpp
@@ -52,7 +52,75 @@ typedef vector<ll> vl;
 typedef vector<P> vp;
 
 const ll INF = 1e9;
-const ll MOD = 1000000007;  // 1e9 + 7
+const ll MOD = 998244353;  // 1e9 + 7
+
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+// int main(int argc, char** argv) {
+//   // int p;
+//   // cin >> p;
+//
+//   MOD = 13;
+//   mint p(10);
+//   cout << (p + 15).x << endl;   // 12 (25 % 13)
+//   cout << (p - 15).x << endl;   // 8  (-5 % 13)
+//   cout << (p * 2).x << endl;    // 7  (20 % 13)
+//   cout << (p.pow(3)).x << endl; // 12 (1000 % 13)
+//   cout << (p / 3).x << endl;    // 12 (12 * 3 = 10 (36 % 13))
+//
+//   mint p2(-3);
+//   cout << p2.x << endl; // 10 (-3 % 13)
+// }
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -60,6 +128,25 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n,s;
+  cin >> n >> s;
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+  }
+
+  // dp[i][j ].. usin[0,i), j is sum.
+  // vector<vector<mint>> dp(n+1, vector<mint>(s+1, 0));
+  // dp[0][0] = 1;
+  vector<mint> dp(s+1, 0);
+  dp[0] = mint(2).pow(n);
+  rep(i,n) {
+    for (ll j = s; j >= 0; --j) { //
+      if (j-a[i] >= 0) {
+        dp[j] += dp[j-a[i]] / 2; // divide by 2 as adding one.
+      }
+    }
+  }
+  //printtree(dp);
+  cout << dp[s] << endl;
 }
