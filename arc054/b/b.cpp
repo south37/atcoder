6b99bb4a0a566948cd8261be35ac80821a05fce8
Nao Minami <south37777@gmail.com>
2020-06-13 11:00:37 +0900

    Add arc054/b

---
+++ arc054/b/b.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cout << setprecision(10) << fixed;
+
+  double p;
+  cin >> p;
+
+  // f[x] .. time when start at x
+  auto f = [&](double x) {
+    return x + p * pow(2,-x/1.5);
+  };
+
+  double l = 0;
+  double r = 1e18;
+  // find the boundary of [f,t]
+  rep(iter,200) {
+    double c1 = l/3*2 + r/3;
+    double c2 = l/3 + r/3*2;
+    if (f(c1) < f(c2)) {
+      r = c2;
+    } else {
+      l = c1;
+    }
+  }
+
+  cout << f(r) << endl;
+}
