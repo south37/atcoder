cea1dfc29e0355cad4f74c920fe150550d8f1822
Nao Minami <south37777@gmail.com>
2020-06-16 00:40:15 +0900

    Add hitachi/d

--- hitachi/d/d.cpp
+++ hitachi/d/d.cpp
@@ -45,7 +45,7 @@ typedef pair<ll, ll> P;
 typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
-const ll INF = 1e9;
+const ll INF = 1e15;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 int main(int argc, char** argv) {
@@ -54,6 +54,68 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n,T;
+  cin >> n >> T;
+  vector<ll> zeros; // b with a = 0
+  vector<P> ps;
+  rep(i,n) {
+    ll a,b;
+    cin >> a >> b;
+    if (a == 0) {
+      zeros.push_back(b);
+    } else {
+      ps.emplace_back(a,b);
+    }
+  }
+  sort(all(ps), [&](const P& p1, const P& p2) {
+    // p1.first/(p1.second+1) > p2.first/(p2.second+1)
+    return p1.first * (p2.second+1) > p2.first * (p1.second+1);
+  });
+  sort(all(zeros));
+
+  // For Debug
+  // for (auto& p : ps) {
+  //   cout << "a,b:" << p.first << "," << p.second << endl;
+  //   cout << (double)p.first/(p.second+1) << endl;
+  // }
+
+  // Here, start DP in ps.
+  // dp[i][j] .. using [0,i), count is j. min cost.
+  ll m = ps.size();
+  // vector<vector<ll>> dp(m+1, vector<ll>(31, INF));
+  vector<ll> dp(31, INF);
+  dp[0] = 0;
+  rep(i,m) {
+    for (ll j = 30; j > 0; --j) {
+      ll t = dp[j-1]; // j > 0
+      if (t == INF) { continue; } // j-1 is invalid.
+      ll ai = ps[i].first;
+      ll bi = ps[i].second;
+      ll now = (ai+1)*t + (ai+bi+1); // cost for achieve j+1
+      if (now > T) { continue; } // t must be equal or lower than T.
+      chmin(dp[j], now);
+    }
+  }
+
+  // printvec(dp);
+
+  ll ans = 0;
+  rep(i,31) {
+    if (dp[i] <= T) {
+      ll now = i; // current value.
+
+      // Here, add b in greedy way.
+      ll t = dp[i];
+      for (ll b : zeros) {
+        ll nt = t + b + 1; // next t
+        if (nt <= T) {
+          t = nt;
+          ++now;
+        }
+      }
+      chmax(ans,now);
+    }
+  }
+
+  cout << ans << endl;
 }
