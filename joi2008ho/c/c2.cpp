8ea2bfc6e5cabd9de7c8f87acd1e62abeea92956
Nao Minami <south37777@gmail.com>
2020-06-17 01:41:18 +0900

    Add joi2008ho/c2

---
+++ joi2008ho/c/c2.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n,m;
+  cin >> n >> m;
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+  }
+
+  set<ll> st; // all pair of i ans j
+  rep(i,n) {
+    if (a[i] <= m) { st.insert(a[i]); }
+    rep(j,i) {
+      if (a[i]+a[j] <= m) { st.insert(a[i]+a[j]); }
+    }
+  }
+
+  ll ans = 0;
+  for (ll v : st) {
+    if (v > m) { continue; }
+
+    ll now = v;
+    ll remain = m-v;
+    // Here, we want maximum value where v <= remain
+    auto it = st.upper_bound(remain);
+    if (it == st.begin()) {
+      // Do nothing
+    } else {
+      now += *prev(it);
+    }
+    chmax(ans, now);
+  }
+  cout << ans << endl;
+}
