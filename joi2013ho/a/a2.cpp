cb9d7647762d5a481c238f91d8b161bf25b72aff
Nao Minami <south37777@gmail.com>
2020-06-18 04:14:41 +0900

    Add a2

---
+++ joi2013ho/a/a2.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define sz(x) (ll)(x).size()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+  }
+
+  vector<ll> b; // length of 1-0-1-...
+  {
+    ll i = 0;
+    while (i<n) {
+      ll j = i+1;
+      while (j<n && a[j] != a[j-1]) {
+        ++j;
+      }
+      // Here, a[j]==a[j-1]. length is j-i.
+      b.emplace_back(j-i);
+      i=j;
+    }
+  }
+
+  // printvec(a);
+  // printvec(b);
+
+  ll ans = 0;
+  ll m = b.size();
+  rep(i,m) {
+    ll now = b[i];
+    if (i>0) {
+      now += b[i-1];
+    }
+    if (i<m-1) {
+      now += b[i+1];
+    }
+    chmax(ans,now);
+  }
+  cout << ans << endl;
+}
