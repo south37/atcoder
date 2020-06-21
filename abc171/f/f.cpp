638bdee38e463493417eabdb74b803ad28cd51b5
Nao Minami <south37777@gmail.com>
2020-06-21 21:48:06 +0900

    Add f

--- abc171/f/f.cpp
+++ abc171/f/f.cpp
@@ -54,12 +54,113 @@ typedef vector<P> vp;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+  // ref. https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+  mint invWithGcd() const {
+    ll a = x, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a/b;
+      a -= t*b; swap(a,b);
+      u -= t*v; swap(u,v);
+    }
+    u %= MOD;
+    if (u < 0) { u += MOD; }
+    return mint(u);
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+// int main(int argc, char** argv) {
+//   // int p;
+//   // cin >> p;
+//
+//   MOD = 13;
+//   mint p(10);
+//   cout << (p + 15).x << endl;    // 12 (25 % 13)
+//   cout << (p - 15).x << endl;    // 8  (-5 % 13)
+//   cout << (p * 2).x << endl;     // 7  (20 % 13)
+//   cout << (p.pow(3)).x << endl;  // 12 (1000 % 13)
+//   cout << (p / 3).x << endl;     // 12 (12 * 3 = 10 (36 % 13))
+//   cout << p * mint(3).invWithGcd().x << endl; // 12 (12 * 3 = 10 (36 % 13))
+//
+//   mint p2(-3);
+//   cout << p2.x << endl; // 10 (-3 % 13)
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll k;
+  cin >> k;
+  string S;
+  cin >> S;
+
+  ll n = S.size();
+  // ll n = 0;
+  // dp[i][j] .. using [0,i),
+  vector<vector<mint>> dp(n+k+5, vector<mint>(n+5));
+  dp[0][0] = 1;
+  rep(i,n+k+1) {
+    rep(j,n+1) {
+      dp[i+1][j+1] += dp[i][j]; // same char
+
+      if (j == n) { // already reached to last
+        dp[i+1][j] += dp[i][j]*26; // we can select all
+      } else {
+        dp[i+1][j] += dp[i][j]*25; // other char
+      }
+    }
+  }
+  cout << dp[n+k][n] << endl;
 }
