edc02b5879430d6d5dc972a8830d0c952c5b3747
Nao Minami <south37777@gmail.com>
2020-07-07 08:56:16 +0900

    Add abc173/e

--- abc173/e/e.cpp
+++ abc173/e/e.cpp
@@ -54,12 +54,154 @@ typedef vector<P> vp;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+  // ref. https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+  mint invWithGcd() const {
+    ll a = x, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a/b;
+      a -= t*b; swap(a,b);
+      u -= t*v; swap(u,v);
+    }
+    u %= MOD;
+    if (u < 0) { u += MOD; }
+    return mint(u);
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+// int main(int argc, char** argv) {
+//   // int p;
+//   // cin >> p;
+//
+//   MOD = 13;
+//   mint p(10);
+//   cout << (p + 15).x << endl;    // 12 (25 % 13)
+//   cout << (p - 15).x << endl;    // 8  (-5 % 13)
+//   cout << (p * 2).x << endl;     // 7  (20 % 13)
+//   cout << (p.pow(3)).x << endl;  // 12 (1000 % 13)
+//   cout << (p / 3).x << endl;     // 12 (12 * 3 = 10 (36 % 13))
+//   cout << p * mint(3).invWithGcd().x << endl; // 12 (12 * 3 = 10 (36 % 13))
+//
+//   mint p2(-3);
+//   cout << p2.x << endl; // 10 (-3 % 13)
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n,k;
+  cin >> n >> k;
+  vector<ll> p; // positive or 0
+  vector<ll> m; // negative
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+    if (a[i] >= 0) { // positive
+      p.push_back(a[i]);
+    } else {
+      m.push_back(a[i]);
+    }
+  }
+  sort(all(p)); // increasinng order. e.g. 1,2,3,4,5,...
+  sort(all(m)); // increasing order. e.g. -5,-4,-3,...
+
+  if (n == k) { // we have to use all
+    mint ans = 1;
+    rep(i,n) {
+      ans *= a[i];
+    }
+    cout << ans << endl;
+  } else {
+    bool isPos = false;
+    if (p.size() > 0) { // Here, we can make plus.
+      isPos = true;
+    } else {
+      isPos = (k%2 == 0); // Here, we can make plus if n is even.
+    }
+
+    mint ans = 1;
+    if (isPos) { // we make plus, take larger value.
+      if (k%2 == 1) {
+        --k;
+        ans = p[p.size()-1];
+        p.pop_back();
+      }
+      // Here, k is even
+
+      vector<ll> muls;
+      reverse(all(p)); // decreaing order
+      rep(i,p.size()/2) {
+        muls.push_back(p[2*i]*p[2*i+1]);
+      }
+      rep(i,m.size()/2) {
+        muls.push_back(m[2*i]*m[2*i+1]);
+      }
+      sort(all(muls));
+      reverse(all(muls));
+
+      rep(i,k/2) {
+        ans *= muls[i];
+      }
+      cout << ans << endl;
+    } else { // we make minus, take small value.
+      // Here, p.size() == 0
+      reverse(all(m)); // decreasing
+      rep(i,k) {
+        ans *= m[i];
+      }
+      cout << ans << endl;
+    }
+  }
 }
