0de43803f8944a2c4d1b9ef1931d44184fecf037
Nao Minami <south37777@gmail.com>
2020-08-05 04:21:42 +0900

    Add abc174/f

--- abc174/f/f.cpp
+++ abc174/f/f.cpp
@@ -54,6 +54,54 @@ typedef vector<P> vp;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+// BIT (Fenwick Tree) with 0-indexed query
+// https://youtu.be/lyHk98daDJo?t=7960
+template<typename T>
+struct BIT {
+  int n;
+  vector<T> d;
+  BIT(int n=0) : n(n), d(n+1) {}
+  void add(int i, T x=1) {
+    i++; // 0-indexed to 1-indexed
+    for (; i <= n; i += i&-i) {
+      d[i] += x;
+    }
+  }
+  // The sum of [0, i]
+  T sum(int i) {
+    i++; // 0-indexed to 1-indexed
+    T x = 0;
+    for (; i; i -= i&-i) {
+      x += d[i];
+    }
+    return x;
+  }
+  // The sum of [l, r)
+  T sum(int l, int r) {
+    assert(l <= r);
+    if (r == 0) { return 0; } // Here, l == 0 && r == 0
+    if (l == 0) { return sum(r-1); }
+    return sum(r-1) - sum(l-1);
+  }
+};
+
+// int main(int argc, char** argv) {
+//   int N;
+//   cin >> N;
+//   vector<int> v(N);
+//   rep(i, N) {
+//     cin >> v[i];
+//   }
+//
+//   BIT<int> bit(N);
+//   rep(i, N) {
+//     bit.add(i, v[i]);
+//   }
+//
+//   cout << bit.sum(4) << endl; // sum of [0,4]. 24
+//   cout << bit.sum(9) << endl; // sum of [0,9]. 55
+// }
+
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
@@ -66,51 +114,32 @@ int main(int argc, char** argv) {
   rep(i,n) {
     cin >> c[i];
   }
-  vector<triple> qs; // (l,r,idx)
+  vector<ll> pos(n+1, -1); // pos[color] .. right most position of color.
+  vector<vector<ll>> rmarks(n); // rmarks[i] .. idx of query at position i.
+  vector<ll> l(q), r(q);
   rep(i,q) {
-    ll l,r;
-    cin >> l >> r;
-    --l; --r;
-    qs.emplace_back(l,r,i);
-  }
-  sort(all(qs));
-  vector<vector<ll>> lmarks(n);
-  vector<vector<ll>> rmarks(n);
-  for (auto& q : qs) {
-    ll l,r,idx;
-    tie(l,r,idx) = q;
-    lmarks[l].push_back(idx);
-    rmarks[r].push_back(idx);
+    cin >> l[i] >> r[i];
+    --l[i]; --r[i]; // 0-indexed
+    rmarks[r[i]].push_back(i);
   }
 
-  vector<set<ll>> cs(q); // cs[i] .. colors in query i.
-
-  vector<set<ll>> nows(n+1);
-  // set<ll> now; // current queries
+  vector<ll> ans(q);
+  BIT<int> bit(n+5);
+  // Start scan.
   rep(i,n) {
-    for (ll idx : lmarks[i]) {
-      rep(i,n+1) {
-        nows[i].insert(idx);
-      }
-      // now.insert(idx);
+    ll nc = c[i]; // current color
+    if (pos[nc] >= 0) { // already inserted
+      bit.add(pos[nc], -1); // erase
     }
+    bit.add(i, 1);
+    pos[nc] = i;
 
-    // Here, now is valid. Update cs.
-    for (ll idx : nows[c[i]-1])  {
-    // for (ll idx : now) {
-      cs[idx].insert(c[i]);
-      nows[c[i]-1].erase(idx);
-    }
-
-    for (ll idx : rmarks[i]) {
-      rep(i,n+1) {
-        nows[i].erase(idx);
-      }
-      // now.erase(idx);
+    for (ll qId : rmarks[i]) {
+      ans[qId] = bit.sum(l[qId], i+1); // sum of [l[qId], i]
     }
   }
 
   rep(i,q) {
-    cout << cs[i].size() << endl;
+    cout << ans[i] << endl;
   }
 }
